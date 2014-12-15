4f60ae9d4d94fdd457c43c6326c102245fc7c2f0
Michael <mgracer48@yahoo.com>
2014-12-15 20:50:30 +0100

    updated files

--- start_game.sh
+++ start_game.sh
@@ -2,13 +2,13 @@ set +v
 read -p "Username: " ttrUsername
 echo "You wrote: $ttrUsername"
 export ttrPassword=password
-export TTI_PLAYCOOKIE=$ttrUsername$
-export TTI_GAMESERVER=108.161.134.133
+export TTR_PLAYCOOKIE=$ttrUsername$
+export TTR_GAMESERVER=108.161.134.133
 source "toontown/toonbase/gameservices.exe"
 echo ===============================
 echo Starting Toontown  World Online Retro...
-echo Username: $ttiUsername$
-echo Client Agent IP: $TTI_GAMESERVER$
+echo Username: $ttrUsername$
+echo Client Agent IP: $TTR_GAMESERVER$
 echo ===============================
 python -m toontown.toonbase.ToontownStart.py
 sleep
