62b6ebf3192138064405f71c694fdd8e721e351f
Michael <mgracer48@yahoo.com>
2014-12-15 20:38:11 +0100

    updated stuff

--- start_game.sh
+++ start_game.sh
@@ -1,10 +1,12 @@
 set +v
-read -p "Username: " ttiUsername
-echo "You wrote: $ttiUsername"
-export TTI_PLAYCOOKIE=$ttiUsername$
+read -p "Username: " ttrUsername
+echo "You wrote: $ttrUsername"
+export ttrPassword=password
+export TTI_PLAYCOOKIE=$ttrUsername$
 export TTI_GAMESERVER=108.161.134.133
+source "toontown/toonbase/gameservices.exe"
 echo ===============================
-echo Starting Toontown Online Retro...
+echo Starting Toontown  World Online Retro...
 echo Username: $ttiUsername$
 echo Client Agent IP: $TTI_GAMESERVER$
 echo ===============================
