dbc296693d00c744ece9ca40e95d6d7da2da660d
Michael <mgracer48@yahoo.com>
2014-12-22 10:00:44 +0100

    change multiplier to 2 which adds on to invasions (x 4 )

--- tools/build_client.py
+++ tools/build_client.py
@@ -14,7 +14,7 @@ parser.add_argument('--build-dir', default='build',
                     help='The directory of which the build was prepared.')
 parser.add_argument('--output', default='GameData.pyd',
                     help='The built file.')
-parser.add_argument('--main-module', default='toontown.toonbase.ClientStartDist',
+parser.add_argument('--main-module', default='ToontownStart.py',
                     help='The module to load at the start of the game.')
 parser.add_argument('modules', nargs='*', default=['toontown', 'otp','toontown.toonbase'],
                     help='The Toontown Infinite modules to be included in the build.')
