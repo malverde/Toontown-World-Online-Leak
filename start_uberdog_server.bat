d53dfede22196e6add3602e6d2e76c1e367adb2b
Reese Jenner <reesevader@hotmail.co.uk>
2015-01-11 19:31:01 +0000

    Cleaned up repository to allow easy starting of the game on gameserver.

---
+++ start_uberdog_server.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set MAX_CHANNELS=999999
+set STATE_SERVER=4002
+set ASTRON_IP=127.0.0.1:7199
+set EVENT_LOGGER_IP=127.0.0.1:7197
+set BASE_CHANNEL=1000000
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+echo ===============================
+echo Starting Toontown Infinite Retro UberDOG server...
+echo ppython: %PPYTHON_PATH%
+echo Base Channel: %BASE_CHANNEL%
+echo Max Channels: %MAX_CHANNELS%
+echo State Server ID: %STATE_SERVER%
+echo Message Director IP: %ASTRON_IP%
+echo Event Logger IP: %EVENT_LOGGER_IP%
+echo ===============================
+
+%PPYTHON_PATH% -m toontown.uberdog.ServiceStart --base-channel %BASE_CHANNEL% ^
+               --max-channels %MAX_CHANNELS% --stateserver %STATE_SERVER% ^
+               --astron-ip %ASTRON_IP% --eventlogger-ip %EVENT_LOGGER_IP%
+
+pause
