bb8d172b6b741c6c0d7a2b2ac16887fc0179a47a
Michael <mgracer48@yahoo.com>
2015-01-15 18:33:40 +0100

    fixed indent errors

--- toontown/election/DistributedSafezoneInvasionAI.py
+++ toontown/election/DistributedSafezoneInvasionAI.py
@@ -362,13 +362,13 @@ class DistributedSafezoneInvasionAI(DistributedObjectAI, FSM):
         suit.dna.newSuit(suitType)
         suit.setSpawnPoint(pointId)
         suit.setLevel(level)
-		attributes = SuitBattleGlobals.SuitAttributes[self.dna.name]
-		self.level = SuitBattleGlobals.pickFromFreqList(attributes['freq'])
+	attributes = SuitBattleGlobals.SuitAttributes[self.dna.name]
+	self.level = SuitBattleGlobals.pickFromFreqList(attributes['freq'])
 
 
         hp = attributes['hp'][self.level] 
         self.maxHP = hp
-	    self.currHP = hp
+	self.currHP = hp
         suit.setHP(self, hp)
         
         suit.generateWithRequired(self.zoneId)
