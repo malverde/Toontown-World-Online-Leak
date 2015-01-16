a46dcffb1275c9af476c45d6cd0ea4afa5264e64
Michael <mgracer48@yahoo.com>
2015-01-16 17:15:52 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/TTW-DEV-Cluster-maintenacefeature' into TTW-DEV-Cluster-Election""
    This reverts commit 3dde2d29775620ec626c9d6fb6a0c1a37f638887.
    
    Conflicts:
    	.gitignore
    	toontown/election/DistributedSafezoneInvasionAI.py

--- toontown/election/DistributedSafezoneInvasionAI.py
+++ toontown/election/DistributedSafezoneInvasionAI.py
@@ -11,16 +11,8 @@ from toontown.suit import SuitTimings
 
 from toontown.toonbase import ToontownBattleGlobals
 from toontown.suit import SuitPlannerBase
-<<<<<<< HEAD
-<<<<<<< HEAD
 #from toontown.suit import SuitBase
 from toontown.suit import DistributedSuitBaseAI
-=======
-from toontown.suit import SuitBase
->>>>>>> parent of 27ec1fe... Merge remote-tracking branch 'origin/TTW-DEV-Cluster-maintenacefeature' into TTW-DEV-Cluster-Election
-=======
-from toontown.suit import SuitBase
->>>>>>> parent of 27ec1fe... Merge remote-tracking branch 'origin/TTW-DEV-Cluster-maintenacefeature' into TTW-DEV-Cluster-Election
 from toontown.battle import SuitBattleGlobals
 from toontown.suit.SuitDNA import SuitDNA
 
