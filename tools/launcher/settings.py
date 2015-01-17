faa72d1d6a22a425ddd33c849a1e08e7408cd18e
Michael <mgracer48@yahoo.com>
2015-01-17 20:34:29 +0100

    updated launcher

--- tools/launcher/settings.py
+++ tools/launcher/settings.py
@@ -1,9 +1,9 @@
 # Launcher-specific settings.
-Version = '1.0' # Set to None if you don't want checks.
-JSONLauncherInfo = "http://download.toontownworldonline.com/launcher/windows/ttw_launcher.json"
+Version = None # Set to None if you don't want checks.
+JSONLauncherInfo = "download.toontownworldonline.com/launcher/windows/ttw_launcher.json"
 
 # Website information
-DefaultDownloadLocation = "http://www.toontownworldonline.com/"
+DefaultDownloadLocation = "www.toontownworldonline.com/"
 
 # Authentication information
 # I warn you, if you try to steal passwords, I will have your head on my lovely pitchfork.
