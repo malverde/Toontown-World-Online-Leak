9e81021cdf191ef151a87c6e3e997547e25fa1ff
Michael <mgracer48@yahoo.com>
2015-01-18 11:14:09 +0100

    worked on group merging

--- toontown/building/DistributedBoardingParty.py
+++ toontown/building/DistributedBoardingParty.py
@@ -127,19 +127,19 @@ class DistributedBoardingParty(DistributedObject.DistributedObject, BoardingPart
             self.notify.debug('new info posted on some other group')
         return
 
-    def postInvite(self, leaderId, inviterId):
+    def postInvite(self, leaderId, inviterId, merger):
         self.notify.debug('post Invite')
         if not base.cr.avatarFriendsManager.checkIgnored(inviterId):
             inviter = base.cr.doId2do.get(inviterId)
             if inviter:
                 if self.inviterPanels.isInvitingPanelUp() or self.inviterPanels.isInvitationRejectedPanelUp():
-                    self.inviterPanels.forceCleanup()
+                 	self.inviterPanels.forceCleanup()
                 self.groupInviteePanel = GroupInvitee.GroupInvitee()
-                self.groupInviteePanel.make(self, inviter, leaderId)
+                self.groupInviteePanel.make(self, inviter, leaderId, merger)
                 if config.GetBool('reject-boarding-group-invites', 0):
                     self.groupInviteePanel.forceCleanup()
                     self.groupInviteePanel = None
-        return
+         	return
 
     def postKick(self, leaderId):
         self.notify.debug('%s was kicked out of the Boarding Group by %s' % (localAvatar.doId, leaderId))
