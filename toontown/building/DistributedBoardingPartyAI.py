77ca3013502d14853893608e33426473c66e0d3e
Michael <mgracer48@yahoo.com>
2015-01-19 00:27:15 +0100

    Revert "improve hover text"
    This reverts commit ec6b8da7b23b47b1ff26eb5331da8bcfe7b0e9c0.

--- toontown/building/DistributedBoardingPartyAI.py
+++ toontown/building/DistributedBoardingPartyAI.py
@@ -397,7 +397,8 @@ class DistributedBoardingPartyAI(DistributedObjectAI.DistributedObjectAI, Boardi
                             return
         if not wantDisableGoButton:
              BoardingPartyBase.BOARDCODE_MISSING, [], []])
-         return
+        return
+
     def testGoButtonRequirements(self, leaderId, elevatorId):
         if self.avIdDict.has_key(leaderId):
             if leaderId == self.avIdDict[leaderId]:
