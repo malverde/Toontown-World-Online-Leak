8021073f5fae8584b768d9e753e5bd8d7b9a8fab
Michael <mgracer48@yahoo.com>
2015-01-24 14:06:18 +0100

    improved maintance message some more

--- otp/otpbase/OTPLocalizerEnglish.py
+++ otp/otpbase/OTPLocalizerEnglish.py
@@ -192,7 +192,7 @@ CRBootedReasons = {1: 'Yikes - An unexpected problem occured.  Your connection h
  151: 'You were kicked out by one of the developers working on the servers.',
  152: "You have been banned from the game for a reported violation of our Terms of Use connected to '%(name)s'. For more details, please check the Toontown World website.",
  153: 'The district you were playing on has been reset.  Everyone who was playing on that district has also been disconnected, however, you should be able to connect again and go right back into Toontown.',
- 154: 'Toontown World is now closed for maintenance. Everyone has been disconnected from the game.\n\nFor more information, please visit the toontown world website.',
+ 154: 'Toontown World is now temporarily closed for maintenance. Everyone who was playing has been disconnected from the game.\n\nFor more information, please visit the Toontown Infinite website.',
  155: 'You\'ve been warned for %(dc_reason)s. Try to behave next time!',
  200: 'Logins are currently disabled. Please try again later.',
  288: 'Sorry, you have used up all of your available minutes this month.',
