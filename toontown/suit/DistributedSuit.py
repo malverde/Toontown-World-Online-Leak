0ed7fa2dfd3f60e2ec1f2ed4d245a6d66cd8ff43
Michael <mgracer48@yahoo.com>
2015-01-24 20:00:14 +0100

    fixed stuff

--- toontown/suit/DistributedSuit.py
+++ toontown/suit/DistributedSuit.py
@@ -330,7 +330,7 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
         numLegs = self.legList.getNumLegs()
         if self.currentLeg != nextLeg:
             self.currentLeg = nextLeg
-	    self.doPathLeg(self.legList.getLeg(nextLeg), elapsed - self.legList.getStartTime(nextLeg)) 
+	    self.doPathLeg(self.legList[nextLeg], elapsed - self.legList.getStartTime(nextLeg))
 	    nextLeg += 1
         if nextLeg < numLegs:
             nextTime = self.legList.getStartTime(nextLeg)
@@ -341,8 +341,8 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
         return Task.done
 
     def doPathLeg(self, leg, time):
-        self.fsm.request(leg.getTypeName(), [leg, time])
-        return 0
+		self.fsm.request(leg.getTypeName(), [leg, time])
+		return 0
 
     def stopPathNow(self):
         name = self.taskName('move')
