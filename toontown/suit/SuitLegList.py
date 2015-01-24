7346050685d6bd3b261bb8ac4a8ec65f1ec7b59c
Michael <mgracer48@yahoo.com>
2015-01-24 22:27:09 +0100

    fixed typo

--- toontown/suit/SuitLegList.py
+++ toontown/suit/SuitLegList.py
@@ -116,7 +116,7 @@ class SuitLegList:
         time = startTime                  
         while legIndex < self.getNumLegs():
             leg = self.legs[legIndex]
-            if leg.getEndTime() > highTime:
+            if leg.getendTime() > highTime:
                 break            
             if (leg.pointA == point) or (leg.pointB == point):
                 return True
