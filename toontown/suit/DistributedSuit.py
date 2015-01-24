af9de937ccd740c356d20b401a5e7ab04e31aea0
Michael <mgracer48@yahoo.com>
2015-01-24 20:37:48 +0100

    fixed stuff i messed up

--- toontown/suit/DistributedSuit.py
+++ toontown/suit/DistributedSuit.py
@@ -330,7 +330,7 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
         numLegs = self.legList.getNumLegs()
         if self.currentLeg != nextLeg:
             self.currentLeg = nextLeg
-	    self.doPathLeg(self.legList[nextLeg], elapsed - self.legList.getStartTime(nextLeg))
+            self.doPathLeg(self.legList.getLeg(nextLeg), elapsed - self.legList.getStartTime(nextLeg))
 	    nextLeg += 1
         if nextLeg < numLegs:
             nextTime = self.legList.getStartTime(nextLeg)
