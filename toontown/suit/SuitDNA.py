625d493a0dc6134fceb18ea5780438cee00c5212
Michael <mgracer48@yahoo.com>
2015-01-25 13:15:24 +0100

    Revert "merge branch 'dev/exitinghqcrashfix'"
    This reverts commit 822e491e593fff9d39ad43a72d6a1d37f3cdbee2, reversing
    changes made to 602db948f4f25a98501f190049aa41f5ae1b34ce.

--- toontown/suit/SuitDNA.py
+++ toontown/suit/SuitDNA.py
@@ -154,6 +154,7 @@ def getRandomSuitByDept(dept):
 class SuitDNA(AvatarDNA.AvatarDNA):
  
     def __init__(self, str = None, type = None, dna = None, r = None, b = None, g = None):
+
         if str != None:
             self.makeFromNetString(str)
         elif type != None:
