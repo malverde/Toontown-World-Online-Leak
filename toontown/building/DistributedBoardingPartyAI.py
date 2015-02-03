89814fc37294f87274082bf38f18402fb2e797e7
Michael <mgracer48@yahoo.com>
2015-02-03 16:49:48 +0100

    add reject decline/cancel cases

--- toontown/building/DistributedBoardingPartyAI.py
+++ toontown/building/DistributedBoardingPartyAI.py
@@ -203,6 +203,8 @@ class DistributedBoardingPartyAI(DistributedObjectAI.DistributedObjectAI, Boardi
 
     def requestCancelInvite(self, inviteeId):
         inviterId = self.air.getAvatarIdFromSender()
+        if inviteeId in self.mergeDict:
+            self.mergeDict.pop(inviteeId)        
         if self.avIdDict.has_key(inviterId):
             leaderId = self.avIdDict[inviterId]
             groupList = self.groupListDict.get(leaderId)
@@ -283,8 +285,9 @@ class DistributedBoardingPartyAI(DistributedObjectAI.DistributedObjectAI, Boardi
     def requestRejectInvite(self, leaderId, inviterId):
         inviteeId = self.air.getAvatarIdFromSender()
         if inviteeId in self.mergeDict:
-            self.mergeDict.pop(inviteeId)  # Do I still want to call removeFromGroup()?  we shall let testing decide        
-        self.removeFromGroup(leaderId, inviteeId)
+            self.mergeDict.pop(inviteeId)
+        else:
+            self.removeFromGroup(leaderId, inviteeId)
         self.sendUpdateToAvatarId(inviterId, 'postInviteDelcined', [inviteeId])
 
     def requestKick(self, kickId):
