c955c8fe048eb5adcd497c516c9b7ed4b242b6e8
Michael <mgracer48@yahoo.com>
2015-02-12 21:33:54 +0100

    fix otpclientrepository

--- start_game.sh
+++ start_game.sh
@@ -1,13 +1,15 @@
-set +v
-export -P ttrUsername="Username: " || ^ 
-echo "You wrote: ttrUsername"
-export -P  ttrPassword="Password: " || ^ 
-export TTR_PLAYCOOKIE=ttrUsername
+set +v 1
+export ttrUsername=mgracer
+//change line 2 when distrubating this, same for start_game.sh
+echo "You wrote: $ttrUsername"
+export ttrPassword=password
+
+export TTR_PLAYCOOKIE=$ttrUsername$
 export TTR_GAMESERVER=54.174.138.210
 
-echo ===============================
-echo Starting Toontown  World Online...
-echo Username: ttrUsername
+echo "==============================="
+echo "Starting Toontown  World Online..."
+echo Username: $ttrUsername$
 echo Client Agent IP: $TTR_GAMESERVER$
 echo ===============================
 ppython -m toontown.toonbase.ToontownStart.py
