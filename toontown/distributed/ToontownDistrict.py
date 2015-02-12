d0dba534db0c52e2595d76f0dd537aba5d24362f
Michael <mgracer48@yahoo.com>
2015-02-12 21:41:53 +0100

    fix district crash

--- toontown/distributed/ToontownDistrict.py
+++ toontown/distributed/ToontownDistrict.py
@@ -9,6 +9,7 @@ class ToontownDistrict(DistributedDistrict.DistributedDistrict):
         DistributedDistrict.DistributedDistrict.__init__(self, cr)
         self.avatarCount = 0
         self.newAvatarCount = 0
+        self.invasionStatus = 0
 
     def allowAHNNLog(self, allow):
         self.allowAHNN = allow
