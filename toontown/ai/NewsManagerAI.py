fb852696840b8d3d0c0ed38d5baa5e4430b0aafa
Michael <mgracer48@yahoo.com>
2015-02-12 18:28:12 +0100

    fix things

--- toontown/ai/NewsManagerAI.py
+++ toontown/ai/NewsManagerAI.py
@@ -1,21 +1,18 @@
-from direct.directnotify import DirectNotifyGlobal
+from direct.directnotify.DirectNotifyGlobal import directNotify
 from direct.distributed.DistributedObjectAI import DistributedObjectAI
-from toontown.toonbase import ToontownGlobals
-from otp.ai.MagicWordGlobal import *
+
 
 class NewsManagerAI(DistributedObjectAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory("NewsManagerAI")
+    notify = directNotify.newCategory('NewsManagerAI')
 
-    def __init__(self, air):
-        DistributedObjectAI.__init__(self, air)
-        self.accept('avatarEntered', self.__announceIfInvasion)
+    def announceGenerate(self):
+        DistributedObjectAI.announceGenerate(self)
 
-    def __announceIfInvasion(self, avatar):
+        self.accept('avatarEntered', self.__handleAvatarEntered)
+
+    def __handleAvatarEntered(self, avatar):
         if self.air.suitInvasionManager.getInvading():
-            self.sendUpdateToAvatarId(avatar.getDoId(), 'setInvasionStatus', [
-                ToontownGlobals.SuitInvasionBulletin, self.air.suitInvasionManager.suitName,
-                self.air.suitInvasionManager.numSuits, self.air.suitInvasionManager.specialSuit
-            ])
+            self.air.suitInvasionManager.notifyInvasionBulletin(avatar.getDoId())
 
     def setPopulation(self, todo0):
         pass
@@ -53,11 +50,11 @@ class NewsManagerAI(DistributedObjectAI):
     def setRoamingTrialerWeekendEnd(self):
         pass
 
-    def setInvasionStatus(self, todo0, todo1, todo2, todo3):
-        pass
+    def setInvasionStatus(self, msgType, cogType, numRemaining, skeleton):
+        self.sendUpdate('setInvasionStatus', args=[msgType, cogType, numRemaining, skeleton])
 
-    def setHolidayIdList(self, todo0):
-        pass
+    def setHolidayIdList(self, holidays):
+        self.sendUpdate('setHolidayIdList', holidays)
 
     def holidayNotify(self):
         pass
@@ -95,10 +92,3 @@ class NewsManagerAI(DistributedObjectAI):
     def sendSystemMessage(self, todo0, todo1):
         pass
 
-@magicWord(category=CATEGORY_DEBUG)
-def invasionstatus():
-    """ Returns the number of cogs available in an invasion in a pretty way. """
-    simbase.air.newsManager.sendUpdateToAvatarId(spellbook.getInvoker().getDoId(), 'setInvasionStatus', [
-        ToontownGlobals.SuitInvasionUpdate, simbase.air.suitInvasionManager.suitName,
-        simbase.air.suitInvasionManager.numSuits, simbase.air.suitInvasionManager.specialSuit
-    ])
