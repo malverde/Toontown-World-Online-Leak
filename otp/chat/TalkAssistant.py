085e34a0601be42d8b93a2a6dbe04bfb6ac3be14
Michael <mgracer48@yahoo.com>
2015-02-14 13:44:57 +0100

    change tti to ttr

--- otp/chat/TalkAssistant.py
+++ otp/chat/TalkAssistant.py
@@ -606,7 +606,7 @@ class TalkAssistant(DirectObject.DirectObject):
 
         message, scrubbed = base.localAvatar.scrubTalk(cleanMessage, modifications)
 
-        base.cr.ttiFriendsManager.sendUpdate('sendTalkWhisper', [receiverAvId, message])
+        base.cr.ttrFriendsManager.sendUpdate('sendTalkWhisper', [receiverAvId, message])
 
     def sendAccountTalk(self, message, receiverAccount):
         error = None
