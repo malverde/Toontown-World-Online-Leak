5df2d07e88efba0935810a6f53a3863cf44f147e
Michael <mgracer48@yahoo.com>
2015-02-15 11:48:00 +0100

    Revert "fix some things"
    This reverts commit ed46b74c8b1d580eb13c00909294f719ade39fa9.

--- mac:linux/start_game.sh
+++ mac:linux/start_game.sh
@@ -1,11 +1,12 @@
 set +v 1
-cd ..
 export ttrUsername=mgracer
 //change line 2 when distrubating this, same for start_game.sh
 echo "You wrote: $ttrUsername"
 export ttrPassword=password
+
 export TTR_PLAYCOOKIE=$ttrUsername$
 export TTR_GAMESERVER=54.174.138.210
+
 echo "==============================="
 echo "Starting Toontown  World Online..."
 echo Username: $ttrUsername$
@@ -13,4 +14,3 @@ echo Client Agent IP: $TTR_GAMESERVER$
 echo ===============================
 ppython -m toontown.toonbase.ToontownStart.py
 sleep 1
-
