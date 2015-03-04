fb23b7ad0e7b873ffb2dd05af3e3bf4ac5eae58f
Michael <mgracer48@yahoo.com>
2015-03-04 20:20:50 +0100

    change it so force skip tutorial is true for now

--- toontown/makeatoon/NameShop.py
+++ toontown/makeatoon/NameShop.py
@@ -1008,10 +1008,9 @@ class NameShop(StateData.StateData):
         self.notify.debug('ParentPos = %.2f %.2f %.2f' % (parentPos[0], parentPos[1], parentPos[2]))
 
     def storeSkipTutorialRequest(self):
-        if base.forceSkipTutorial:
-            base.cr.skipTutorialRequest = True
-        else:
-            base.cr.skipTutorialRequest = self.requestingSkipTutorial
+       # if base.forceSkipTutorial:
+        base.cr.skipTutorialRequest = True
+            #base.cr.skipTutorialRequest = self.requestingSkipTutorial
 
     def __isFirstTime(self):
         if not self.makeAToon.nameList or self.makeAToon.warp:
