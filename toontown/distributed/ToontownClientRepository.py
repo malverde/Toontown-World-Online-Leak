be164177bf449212df81f29b4d92f7e6b6460152
Michael <mgracer48@yahoo.com>
2015-03-05 16:15:45 +0100

    fix beginbulkload

--- toontown/distributed/ToontownClientRepository.py
+++ toontown/distributed/ToontownClientRepository.py
@@ -351,7 +351,7 @@ class ToontownClientRepository(OTPClientRepository.OTPClientRepository):
         self.cleanupWaitingForDatabase()
         dclass = self.dclassesByName['DistributedToon']
         NametagGlobals.setMasterArrowsOn(0)
-        loader.beginBulkLoad('localAvatarPlayGame', OTPLocalizer.CREnteringToontown, 400, 1, TTLocalizer.TIP_GENERAL)
+        loader.beginBulkLoad('localAvatarPlayGame', OTPLocalizer.CREnteringToontown, 400, 1, TTLocalizer.TIP_GENERAL, 0)
         localAvatar = LocalToon.LocalToon(self)
         localAvatar.dclass = dclass
         base.localAvatar = localAvatar
