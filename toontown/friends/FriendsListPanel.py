7852d9afa31fc266a005b5ecd56187dfc35ac5f8
Michael <mgracer48@yahoo.com>
2015-03-06 14:14:13 +0100

    fix exiting friend list crash

--- toontown/friends/FriendsListPanel.py
+++ toontown/friends/FriendsListPanel.py
@@ -54,7 +54,6 @@ def showFriendsList():
     global globalFriendsList
     if globalFriendsList == None:
         globalFriendsList = FriendsListPanel()
-    base.hideFriendMargins()
     globalFriendsList.enter()
     return
 
@@ -300,7 +299,7 @@ class FriendsListPanel(DirectFrame, StateData.StateData):
         self.ignore(OTPGlobals.PlayerFriendUpdateEvent)
         base.localAvatar.obscureFriendsListButton(-1)
         messenger.send(self.doneEvent)
-        base.showFriendMargins()
+        
         return None
 
     def __close(self):
