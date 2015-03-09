4c329d0e041322d80260d796f532c9eb12eca8cc
Michael <mgracer48@yahoo.com>
2015-03-09 18:29:28 +0100

    fix trunk

--- toontown/estate/DistributedFurnitureManagerAI.py
+++ toontown/estate/DistributedFurnitureManagerAI.py
@@ -98,7 +98,15 @@ class DistributedFurnitureManagerAI(DistributedObjectAI):
         self.items = []
 
         for item in items:
-            if item.getFlags() & FLCloset:
+            if item.getFlags() & FLTrunk:
+                if self.house.gender is 0:
+                    if item.furnitureType - 4000 < 10:
+                        item.furnitureType += 10
+                elif item.furnitureType - 4000 > 10:
+                    item.furnitureType -= 10
+                do = DistributedTrunkAI(self.air, self, item)
+        
+            elif item.getFlags() & FLCloset:
                 if self.house.gender is 0:
                     # If they have a male closet, we need to make it a female closet.
                     if item.furnitureType - 500 < 10:
@@ -111,13 +119,7 @@ class DistributedFurnitureManagerAI(DistributedObjectAI):
                 do = DistributedBankAI(self.air, self, item)
             elif item.getFlags() & FLPhone:
                 do = DistributedPhoneAI(self.air, self, item)
-            elif item.getFlags() & FLTrunk:
-                if self.house.gender is 0:
-                    if item.furnitureType - 500 > 10:
-                        item.furnitureType += 10
-                elif item.furnitureType - 500 > 10:
-                    item.furnitureType -=10
-                do = DistributedTrunkAI(self.air, self, item)
+
             else:
                 do = DistributedFurnitureItemAI(self.air, self, item)
 
@@ -275,8 +277,15 @@ class DistributedFurnitureManagerAI(DistributedObjectAI):
         self.d_setAtticItems(self.getAtticItems())
 
         item.posHpr = (x, y, z, h, p, r)
+        if item.getFlags() & FLTrunk:
+            if self.house.gender is 0:
+                if item.furnitureType - 4000 < 10:
+                    item.furnitureType += 10
+            elif item.furnitureType - 4000 > 10:
+                item.furnitureType -= 10
+            do = DistributedTrunkAI(self.air, self, item)
 
-        if item.getFlags() & FLCloset:
+        elif item.getFlags() & FLCloset:
             if self.house.gender is 0:
                 # If they have a male closet, we need to make it a female closet.
                 if item.furnitureType - 500 < 10:
