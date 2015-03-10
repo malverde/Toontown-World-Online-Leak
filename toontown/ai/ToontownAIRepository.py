e828f3762f3e7447ddf51180ed60c621a99412e4
Michael <mgracer48@yahoo.com>
2015-03-10 18:40:53 +0100

    add missing wantpets

--- toontown/ai/ToontownAIRepository.py
+++ toontown/ai/ToontownAIRepository.py
@@ -93,6 +93,7 @@ class ToontownAIRepository(ToontownInternalRepository):
         self.doLiveUpdates = self.config.GetBool('want-live-updates', True)
         self.baseXpMultiplier = self.config.GetFloat('base-xp-multiplier', 1.0)
         self.wantCogdominiums = self.config.GetBool('want-cogdominiums', True)
+        self.wantPets = self.config.GetBool('want-pets', True)
         self.holidayManager = HolidayManagerAI(self)
 
         self.fishManager = FishManagerAI()
