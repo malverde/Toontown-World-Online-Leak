7ac61c5d95093732fd080be75a53769c2a7bbce8
Michael <mgracer48@yahoo.com>
2015-03-11 17:48:16 +0100

    fix things

--- toontown/friends/FriendsListManager.py
+++ toontown/friends/FriendsListManager.py
@@ -96,7 +96,6 @@ class FriendsListManager:
         FriendsListPanel.showFriendsList()
 
     def __handleClickedNametag(self, avatar, playerId = None):
-        base.hideFriendMargins()
         self.notify.debug('__handleClickedNametag. doId = %s' % avatar.doId)
         if avatar.isPet():
             self.avatarPanel = PetAvatarPanel.PetAvatarPanel(avatar)
