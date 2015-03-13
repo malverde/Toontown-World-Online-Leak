cd89d8aaa654db652a511f3a4f287d2a193d5c13
Michael <mgracer48@yahoo.com>
2015-03-13 17:04:53 +0100

    fix things

--- toontown/distributed/ToontownClientRepository.py
+++ toontown/distributed/ToontownClientRepository.py
@@ -373,28 +373,23 @@ class ToontownClientRepository(OTPClientRepository.OTPClientRepository):
         self.loginFSM.request('playingGame')
 
     def getAvatarDetails(self, avatar, func, *args):
-        avId = avatar.doId
-        if avId in self.doId2do:
-            func(1, self.doId2do[avId], *args)
-            return
-
         pad = ScratchPad()
         pad.func = func
         pad.args = args
         pad.avatar = avatar
         pad.delayDelete = DelayDelete.DelayDelete(avatar, 'getAvatarDetails')
-        self.__queryAvatarMap[avId] = pad
-        self.__sendGetAvatarDetails(avId)
+        self.__queryAvatarMap[avatar.doId] = pad
+        self.__sendGetAvatarDetails(avatar.doId)
 
     def cancelAvatarDetailsRequest(self, avatar):
         avId = avatar.doId
-        if self.__queryAvatarMap.has_key(avId):
+        if avId in self.__queryAvatarMap:
             pad = self.__queryAvatarMap.pop(avId)
             pad.delayDelete.destroy()
 
     def __sendGetAvatarDetails(self, avId):
-        #return
         self.ttrFriendsManager.d_getAvatarDetails(avId)
+
     def n_handleGetAvatarDetailsResp(self, avId, fields):
         self.notify.info('Query reponse for avId %d' % avId)
         try:
