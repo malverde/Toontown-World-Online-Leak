b516abf3f8c1249cbf17206fc037e283e324ccae
Michael <mgracer48@yahoo.com>
2015-03-13 22:17:50 +0100

    fix sellbothq boss battle

--- toontown/coghq/CogHQBossBattle.py
+++ toontown/coghq/CogHQBossBattle.py
@@ -5,8 +5,8 @@ from toontown.hood import Place
 from direct.fsm import ClassicFSM, State
 from direct.fsm import State
 from otp.distributed.TelemetryLimiter import RotationLimitToH, TLGatherAllAvs
-from toontown.nametag.NametagGlobals import *
-from toontown.nametag import NametagGlobals
+from otp.nametag.NametagConstants import *
+from otp.nametag import NametagGlobals
 from toontown.toonbase import ToontownGlobals
 from toontown.toonbase import ToontownBattleGlobals
 from toontown.battle import BattlePlace
@@ -112,7 +112,7 @@ class CogHQBossBattle(BattlePlace.BattlePlace):
         if self.bossCog:
             self.bossCog.d_avatarEnter()
         self._telemLimiter = TLGatherAllAvs('CogHQBossBattle', RotationLimitToH)
-        NametagGlobals.setWant2dNametags(True)
+        NametagGlobals.setMasterArrowsOn(1)
         base.localAvatar.inventory.setRespectInvasions(0)
         self.fsm.request(requestStatus['how'], [requestStatus])
 
@@ -162,7 +162,6 @@ class CogHQBossBattle(BattlePlace.BattlePlace):
 
     def enterOuch(self):
         base.localAvatar.setTeleportAvailable(0)
-        #I think this is what we need to use for the laff during battle :)
         base.localAvatar.laffMeter.start()
 
     def exitOuch(self):
