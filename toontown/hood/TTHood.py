f93fa62cced7ddd2fdeab0d503770b45d94dd68a
Michael <mgracer48@yahoo.com>
2015-03-14 18:52:15 +0100

    fix tthood

--- toontown/hood/TTHood.py
+++ toontown/hood/TTHood.py
@@ -16,6 +16,8 @@ class TTHood(ToonHood):
     SKY_FILE = 'phase_3.5/models/props/TT_sky'
     SPOOKY_SKY_FILE = 'phase_3.5/models/props/BR_sky'
     TITLE_COLOR = (1.0, 0.5, 0.4, 1.0)
+    loader = TTTownLoader
+    self.loader = TTTownLoader
 
     HOLIDAY_DNA = {
       ToontownGlobals.WINTER_DECORATIONS: ['phase_4/dna/winter_storage_TT.pdna', 'phase_4/dna/winter_storage_TT_sz.pdna'],
@@ -24,22 +26,3 @@ class TTHood(ToonHood):
       ToontownGlobals.SPOOKY_PROPS: ['phase_4/dna/halloween_props_storage_TT.pdna', 'phase_4/dna/halloween_props_storage_TT_sz.pdna']}
 
 
-@magicWord(category=CATEGORY_MODERATION)
-def spooky():
-    """
-    Activates the 'spooky' effect on the current area.
-    """
-    hood = base.cr.playGame.hood
-    if not hasattr(hood, 'startSpookySky'):
-        return "Couldn't find spooky sky."
-    if hasattr(hood, 'magicWordSpookyEffect'):
-        return 'The spooky effect is already active!'
-    hood.magicWordSpookyEffect = True
-    hood.startSpookySky()
-    fadeOut = base.cr.playGame.getPlace().loader.geom.colorScaleInterval(
-        1.5, Vec4(0.55, 0.55, 0.65, 1), startColorScale=Vec4(1, 1, 1, 1),
-        blendType='easeInOut')
-    fadeOut.start()
-    spookySfx = base.loadSfx('phase_4/audio/sfx/spooky.ogg')
-    spookySfx.play()
-    return 'Activating the spooky effect...'
