8ee3bfe891b425c2b48e338774016e9d974f792a
Michael <mgracer48@yahoo.com>
2015-03-14 14:31:43 +0100

    fix catalog

--- toontown/catalog/CatalogScreen.py
+++ toontown/catalog/CatalogScreen.py
@@ -975,41 +975,13 @@ class CatalogScreen(DirectFrame):
         self.responseDialog = None
         return
 
-    def setClarabelleChat(self, str, timeout = 6, type = None):
+    def setClarabelleChat(self, str, timeout = 6):
         self.clearClarabelleChat()
-
-        # Clarabelle can talk now! Let's give her some variation.
-        if type == 'greeting':
-            if not self.clarabelleGreetingSfx:
-                clarabelleGreeting1 = base.loadSfx('phase_5.5/audio/dial/clarabelle_ah_1.ogg')
-                clarabelleGreeting2 = base.loadSfx('phase_5.5/audio/dial/clarabelle_ah_2.ogg')
-                self.clarabelleGreetingSfx = [clarabelleGreeting1, clarabelleGreeting2]
-            base.playSfx(random.choice(self.clarabelleGreetingSfx))
-        elif type == 'goodbye':
-            if not self.clarabelleGoodbyeSfx:
-                clarabelleGoodbye1 = base.loadSfx('phase_5.5/audio/dial/clarabelle_wa_2.ogg')
-                clarabelleGoodbye2 = base.loadSfx('phase_5.5/audio/dial/clarabelle_wa_3.ogg')
-                clarabelleGoodbye3 = base.loadSfx('phase_5.5/audio/dial/clarabelle_wa_4.ogg')
-                self.clarabelleGoodbyeSfx = [clarabelleGoodbye1, clarabelleGoodbye2, clarabelleGoodbye3]
-            base.playSfx(random.choice(self.clarabelleGoodbyeSfx))
-        elif type == 'error':
-            if not self.clarabelleErrorSfx:
-                clarabelleError1 = base.loadSfx('phase_5.5/audio/dial/clarabelle_ah_2.ogg')
-                self.clarabelleChatterSfx = [clarabelleError1]
-            base.playSfx(random.choice(self.clarabelleErrorSfx))
-        else:
-            # These are currently placeholders.
-            if not self.clarabelleChatterSfx:
-                clarabelleChatter1 = base.loadSfx('phase_5.5/audio/dial/clarabelle_ah_2.ogg')
-                clarabelleChatter2 = base.loadSfx('phase_5.5/audio/dial/clarabelle_gen_2.ogg')
-                self.clarabelleChatterSfx = [clarabelleChatter1, clarabelleChatter2]
-            base.playSfx(random.choice(self.clarabelleChatterSfx))
-
         if not self.clarabelleChatBalloon:
-            self.clarabelleChatBalloon = loader.loadModel('phase_3/models/props/chatbox')
+            self.clarabelleChatBalloon = loader.loadModel('phase_3/models/props/chatbox.bam')
         self.clarabelleChat = ChatBalloon(self.clarabelleChatBalloon)
-        chatNode = self.clarabelleChat.generate(str, ToontownGlobals.getInterfaceFont())[0]
-        self.clarabelleChatNP = self.attachNewNode(chatNode.node(), 1000)
+        chatNode = self.clarabelleChat.generate(str, ToontownGlobals.getInterfaceFont())
+        self.clarabelleChatNP = chatNode
         self.clarabelleChatNP.setScale(0.08)
         self.clarabelleChatNP.setPos(0.7, 0, 0.6)
         if timeout:
