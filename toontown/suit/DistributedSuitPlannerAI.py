bc67fcc7c57e6938d7724a6b6d183e0fcb62e542
Michael <mgracer48@yahoo.com>
2015-03-14 17:33:02 +0100

    Revert "fix suits dissapearing"
    This reverts commit 5bbf5fd34bbb3737c321ba48b4dee488674df69d.

--- toontown/suit/DistributedSuitPlannerAI.py
+++ toontown/suit/DistributedSuitPlannerAI.py
@@ -12,8 +12,9 @@ from toontown.battle import BattleManagerAI
 from toontown.battle import SuitBattleGlobals
 from toontown.building import HQBuildingAI
 from toontown.building import SuitBuildingGlobals
-from libpandadna.DNAParser import DNASuitPoint
+from toontown.dna.DNAParser import DNASuitPoint
 from toontown.hood import ZoneUtil
+from toontown.suit.SuitInvasionGlobals import IFSkelecog, IFWaiter, IFV2
 from toontown.suit.SuitLegList import *
 from toontown.toon import NPCToons
 from toontown.toonbase import ToontownBattleGlobals
