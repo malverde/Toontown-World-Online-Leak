36d0116dcd7ff6396d18aee4d6e44b19fc5f255a
Michael <mgracer48@yahoo.com>
2015-03-15 00:46:06 +0100

    Revert "fix suit"
    This reverts commit 8c25c5469f3bbdf2e513de10d6d92a9f4f11a5b3.

--- toontown/suit/SuitBase.py
+++ toontown/suit/SuitBase.py
@@ -77,7 +77,8 @@ class SuitBase:
             self.notify.warning('called getActualLevel with no DNA, returning 1 for level')
             return 1
 
-    def setPath(self, path):
+    def setPath(self, suitGraph, path):
+        self.suitGraph = suitGraph
         self.path = path
 
     def getPath(self):
