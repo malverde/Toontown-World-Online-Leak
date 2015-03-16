96bccc5e708990452694f3f78a78237ad6970c48
Michael <mgracer48@yahoo.com>
2015-03-16 15:40:19 +0100

    use public_client.prc in prepare_client

--- tools/prepare_client.py
+++ tools/prepare_client.py
@@ -13,7 +13,7 @@ parser.add_argument('--build-dir', default='build',
                     help='The directory in which to store the build files.')
 parser.add_argument('--src-dir', default='..',
                     help='The directory of the Toontown source code.')
-parser.add_argument('--server-ver', default='ttw-pre-alpha-dev-build-2.5.0',
+parser.add_argument('--server-ver', default='ttw-pre-alpha-2.5.0',
                     help='The server version of this build.\n')
 parser.add_argument('--build-mfs', action='store_true',
                     help='When present, multifiles will be built.')
@@ -108,7 +108,7 @@ for module in args.modules:
 # PRC file data, (stripped) DC file, and time zone info.
 
 # First, we need the PRC file data:
-configFileName = 'dev.prc'
+configFileName = 'public_client.prc'
 configData = []
 with open(os.path.join(args.src_dir, 'config', configFileName)) as f:
     data = f.read()
