750f9cfcef5c4676b897b5860e8a482b80ab4e17
daviddave <david.c.5@hotmail.co.uk>
2015-03-17 20:14:46 +0000

    Update davidsgameupdater.py

--- davidsgameupdater.py
+++ davidsgameupdater.py
@@ -1,85 +1,76 @@
-from direct.interval.IntervalGlobal import Sequence
-from sys import argv
-from direct.directbase import DirectStart
-from direct.task import Task
-from direct.actor.Actor import Actor
-from direct.gui.DirectGui import *
-from pandac.PandaModules import *
-from direct.interval.IntervalGlobal import *
-
-from direct.controls.GravityWalker import GravityWalker
-
-from direct.interval.IntervalGlobal import *
-import urllib, os, __main__, random
-from pandac.PandaModules import *
-from random import choice
-base.disableMouse()
-from direct.task import Task
-import math
-from math import pi, sin, cos
-
-from direct.task import Task
-from direct.interval.IntervalGlobal import *
-from pandac.PandaModules import *
-
-from direct.gui.DirectGui import *
-from direct.actor.Actor import Actor
-
-from direct.filter.CommonFilters import *
-import sys
-from random import randint
-import direct.directbase.DirectStart
-from panda3d.core import *
-from direct.gui.OnscreenText import OnscreenText
-
 import os
-
+import urllib 
 
 if (os.path.exists('GameData.pyd')):
 	os.unlink('GameData.pyd')
- 
-def downloadTask(task):
 	
-	if channel.run():
-		# Still waiting for file to finish downloading.
-		return task.cont
-	if not channel.isDownloadComplete():
-		print "Error downloading file."
-		return task.done
-	data = rf.getData()
-	print "got data:"
-	print data
-	return task.done
-http = HTTPClient()
-channel = http.makeChannel(True)
-channel.beginGetDocument(DocumentSpec('https://toontownworldonline.com/download/GameData.pyd'))
-rf = Ramfile()
-channel.downloadToRam(rf)
-channel.downloadToFile('GameData.pyd')
-taskMgr.add(downloadTask, 'download')
-
-import os
-
-
 if (os.path.exists('GameData.so')):
 	os.unlink('GameData.so')
- 
-def downloadTask(task):
+
+if (os.path.exists('phase_1.mf')):
+	os.unlink('phase_1.mf')
+
+if (os.path.exists('phase_2.mf')):
+	os.unlink('phase_2.mf')
+
+if (os.path.exists('phase_3.mf')):
+	os.unlink('phase_3.mf')
+
+if (os.path.exists('phase_4.mf')):
+	os.unlink('phase_4.mf')
 	
-	if channel.run():
-		# Still waiting for file to finish downloading.
-		return task.cont
-	if not channel.isDownloadComplete():
-		print "Error downloading file."
-		return task.done
-	data = rf.getData()
-	print "got data:"
-	print data
-	return task.done
-http = HTTPClient()
-channel = http.makeChannel(True)
-channel.beginGetDocument(DocumentSpec('https://toontownworldonline.com/download/GameData.so'))
-rf = Ramfile()
-channel.downloadToRam(rf)
-channel.downloadToFile('GameData.so')
-taskMgr.add(downloadTask, 'download')
+if (os.path.exists('phase_3.5.mf')):
+	os.unlink('phase_3.5.mf')
+	
+if (os.path.exists('phase_5.mf')):
+	os.unlink('phase_5.mf')
+	
+if (os.path.exists('phase_5.5.mf')):
+	os.unlink('phase_5.5.mf')
+	
+if (os.path.exists('phase_6.mf')):
+	os.unlink('phase_6.mf')
+	
+if (os.path.exists('phase_7.mf')):
+	os.unlink('phase_7.mf')
+	
+if (os.path.exists('phase_8.mf')):
+	os.unlink('phase_8.mf')
+	
+if (os.path.exists('phase_9.mf')):
+	os.unlink('phase_9.mf')
+	
+if (os.path.exists('phase_10.mf')):
+	os.unlink('phase_10.mf')
+	
+if (os.path.exists('phase_11.mf')):
+	os.unlink('phase_11.mf')
+	
+if (os.path.exists('phase_12.mf')):
+	os.unlink('phase_12.mf')
+	
+if (os.path.exists('phase_13.mf')):
+	os.unlink('phase_13.mf')
+	
+if (os.path.exists('_miraidata.py')):
+	os.unlink('_miraidata.py')
+	
+
+f = open('_miraidata.py','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/_miraidata.py').read()); f.close()
+f = open('phase_10.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_10.mf').read()); f.close()
+f = open('phase_1.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_1.mf').read()); f.close()
+f = open('Gamedata.pyd','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/GameData.pyd').read()); f.close()
+f = open('GameData.so','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/GameData.so').read()); f.close()
+f = open('phase_12.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_2.mf').read()); f.close()
+f = open('phase_3.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_3.mf').read()); f.close()
+f = open('phase_3.5.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_3.5.mf').read()); f.close()
+f = open('phase_4.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_4.mf').read()); f.close()
+f = open('phase_5.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_5.mf').read()); f.close()
+f = open('phase_5.5.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_5.5.mf').read()); f.close()
+f = open('phase_6.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_6.mf').read()); f.close()
+f = open('phase_7.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_7.mf').read()); f.close()
+f = open('phase_8.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_8.mf').read()); f.close()
+f = open('phase_9.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_9.mf').read()); f.close()
+f = open('phase_11.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_11.mf').read()); f.close()
+f = open('phase_12.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_12.mf').read()); f.close()
+f = open('phase_13.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_13.mf').read()); f.close()
