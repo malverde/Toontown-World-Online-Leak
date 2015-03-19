4ea47ec002925471fb4a7065a8353e5b57f116e5
daviddave <david.c.5@hotmail.co.uk>
2015-03-19 18:59:54 +0000

    Update davidsgameupdater.py
    Fixed!!

--- davidsgameupdater.py
+++ davidsgameupdater.py
@@ -8,15 +8,15 @@ print 'Removing old files'
 if platform == 'win32':
 	if (os.path.exists('GameData.pyd')):
 		os.unlink('GameData.pyd')
-else:	
+if platform == 'darwin':
 	if (os.path.exists('GameData.so')):
 		os.unlink('GameData.so')	
 
 print 'updating  game'
 if platform == 'win32':
 	f = open('GameData.pyd','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/GameData.pyd').read()); f.close()
-else:
+if platform == 'darwin':
 	f = open('GameData.so','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/GameData.so').read()); f.close()
-	print 'updated the gamedata'
+print 'updated the gamedata'
 
 print 'Main game updated!'
