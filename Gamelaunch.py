2ae448c52b1b29a228d408144353ca0abaece6ec
Michael <mgracer48@yahoo.com>
2015-03-19 15:18:53 +0100

    fix gamelaunch

--- Gamelaunch.py
+++ Gamelaunch.py
@@ -1,8 +1,7 @@
 import os 
-username = = raw_input('Username: ' )
+username = raw_input('Username: ' )
 #password = raw_input('Password: ')
 os.environ['TTR_PLAYCOOKIE'] =  username
-os.environ['TTR_GAMESERVER'] = '52.0.191.143'
 import panda3d.core 
 import GameData
 import toontown.toonbase.ToontownStartDist
