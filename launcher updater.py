2b38976a2ffcbbb99e54c4b8cfb7802244abf271
mgracer48 <mgracer48@users.noreply.github.com>
2015-03-22 14:16:12 +0100

    Update launcher updater.py

--- launcher updater.py
+++ launcher updater.py
@@ -16,18 +16,18 @@ if (os.path.exists('phaseupdater.py')):
 if (os.path.exists('phase_2.mf')):
 	os.unlink('phase_2.mf')
 
-print 'patching phase updater'
+print 'updating phase updater'
 f = open('phaseupdater.py','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phaseupdater.py').read()); f.close()
 print 'patched phase updater!'
 
-print 'patching game updater'
+print 'updating game updater'
 f = open('gameupdater.py','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/gameupdater.py').read()); f.close()
 print 'patched game updater!'
 
-print 'patching launcher'
+print 'updating launcher'
 f = open('launcher.py','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/launcher.py').read()); f.close()
 print 'patched launcher!'
 
-print 'patching phase_2'
+print 'updating phase_2'
 f = open('phase_2.mf','wb'); f.write(urllib.urlopen('https://toontownworldonline.com/download/phase_2.mf').read()); f.close()
 print 'patched phase_2!'
