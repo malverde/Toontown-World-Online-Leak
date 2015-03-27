41d9b1045267fa92ee049201f74eabb4ea348852
Michael <mgracer48@yahoo.com>
2015-03-27 19:39:19 +0100

    change distributedsuit back to normal

--- toontown/suit/DistributedSuit.py
+++ toontown/suit/DistributedSuit.py
@@ -1,29 +1,33 @@
-from pandac.PandaModules import *
-from direct.interval.IntervalGlobal import *
-from direct.distributed.ClockDelta import *
+import copy
+from direct.directnotify import DirectNotifyGlobal
 from direct.directtools.DirectGeometry import CLAMP
-from direct.task import Task
-from otp.avatar import DistributedAvatar
-import Suit
-from toontown.toonbase import ToontownGlobals
-from toontown.battle import DistributedBattle
+from direct.distributed.ClockDelta import *
 from direct.fsm import ClassicFSM, State
 from direct.fsm import State
-import SuitTimings
-import SuitBase
-import DistributedSuitPlanner
-from direct.directnotify import DirectNotifyGlobal
-import SuitDialog
-from toontown.battle import BattleProps
-from toontown.distributed.DelayDeletable import DelayDeletable
+from direct.interval.IntervalGlobal import *
+from direct.task import Task
 import math
-import copy
-import DistributedSuitBase
-from otp.otpbase import OTPLocalizer
+from pandac.PandaModules import *
 import random
-from SuitLegList import *
-from otp.nametag.NametagConstants import *
-from otp.nametag import NametagGlobals
+
+import DistributedSuitBase
+import DistributedSuitPlanner
+import Suit
+import SuitBase
+import SuitDialog
+import SuitTimings
+from otp.avatar import DistributedAvatar
+from otp.otpbase import OTPLocalizer
+from toontown.battle import BattleProps
+from toontown.battle import DistributedBattle
+from toontown.chat.ChatGlobals import *
+from toontown.distributed.DelayDeletable import DelayDeletable
+from toontown.nametag import NametagGlobals
+from toontown.nametag.NametagGlobals import *
+from toontown.suit.SuitLegList import *
+from toontown.toonbase import ToontownGlobals
+
+
 STAND_OUTSIDE_DOOR = 2.5
 BATTLE_IGNORE_TIME = 6
 BATTLE_WAIT_TIME = 3
@@ -330,7 +334,7 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
         numLegs = self.legList.getNumLegs()
         if self.currentLeg != nextLeg:
             self.currentLeg = nextLeg
-            self.doPathLeg(self.legList[nextLeg], elapsed - self.legList.getStartTime(nextLeg))
+            self.doPathLeg(self.legList.getLeg(nextLeg), elapsed - self.legList.getStartTime(nextLeg))
         nextLeg += 1
         if nextLeg < numLegs:
             nextTime = self.legList.getStartTime(nextLeg)
@@ -341,7 +345,7 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
         return Task.done
 
     def doPathLeg(self, leg, time):
-        self.fsm.request(SuitLeg.getTypeName(leg.getType()), [leg, time])
+        self.fsm.request(leg.getTypeName(), [leg, time])
         return 0
 
     def stopPathNow(self):
@@ -630,12 +634,11 @@ class DistributedSuit(DistributedSuitBase.DistributedSuitBase, DelayDeletable):
             base.playSfx(dialogue, node=self)
         elif chatFlags & CFSpeech != 0:
             if self.nametag.getNumChatPages() > 0:
-                self.playDialogueForString(self.nametag.getChat())
+                self.playDialogueForString(self.nametag.getChatText())
                 if self.soundChatBubble != None:
                     base.playSfx(self.soundChatBubble, node=self)
-            elif self.nametag.getChatStomp() > 0:
-                self.playDialogueForString(self.nametag.getStompText(), self.nametag.getStompDelay())
-        return
+            elif self.nametag.getStompChatText():
+                self.playDialogueForString(self.nametag.getStompChatText(), self.nametag.CHAT_STOMP_DELAY)
 
     def playDialogueForString(self, chatString, delay = 0.0):
         if len(chatString) == 0:
