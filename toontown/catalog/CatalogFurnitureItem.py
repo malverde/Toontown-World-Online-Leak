159686cdeaa0572485d138dbe1922ed2532a40ec
voarsh <reesevader@hotmail.co.uk>
2015-04-10 10:06:11 +0100

    Attempted to fix an indent error

--- toontown/catalog/CatalogFurnitureItem.py
+++ toontown/catalog/CatalogFurnitureItem.py
@@ -1103,10 +1103,11 @@ class CatalogFurnitureItem(CatalogAtticItem.CatalogAtticItem):
         CatalogAtticItem.CatalogAtticItem.encodeDatagram(self, dg, store)
         dg.addInt16(self.furnitureType)
         if FurnitureTypes[self.furnitureType][FTColorOptions]:
-            if self.colorOption == None:
-                     dg.addUint8(0)
-                 else:
-                     dg.addUint8(self.colorOption)
+            if store & CatalogItem.Customization:
+                if self.colorOption == None:
+                    dg.addUint8(0)
+                else:
+                    dg.addUint8(self.colorOption)
 
     def getAcceptItemErrorText(self, retcode):
         if retcode == ToontownGlobals.P_AlreadyOwnBiggerCloset:
