8bbb1eedf1bd0e14e2602690760f801b452d5a40
voarsh <reesevader@hotmail.co.uk>
2015-04-16 23:10:15 +0000

    Removed crappy build files

--- tools/build/otp/nametag/NametagFloat2d.py
+++
@@ -1,8 +0,0 @@
-# Embedded file name: otp\nametag\NametagFloat2d.py
-from Nametag3d import *
-
-class NametagFloat2d(Nametag3d):
-    WANT_DYNAMIC_SCALING = False
-    SCALING_FACTOR = 1.0
-    SHOULD_BILLBOARD = False
-    IS_3D = False
