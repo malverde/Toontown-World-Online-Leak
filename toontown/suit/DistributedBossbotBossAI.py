a2ae89adc748ee7e63084dd00f9d5d732fe0a2bf
voarsh <reesevader@hotmail.co.uk>
2015-05-27 21:22:37 +0100

    Tried to define INVOKER for FEEDCOGS MW

--- toontown/suit/DistributedBossbotBossAI.py
+++ toontown/suit/DistributedBossbotBossAI.py
@@ -38,6 +38,7 @@ class DistributedBossbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
         self.numTables = 1
         self.numDinersPerTable = 3
         self.tables = []
+		self.invoker = []
         self.numGolfSpots = 4
         self.golfSpots = []
         self.toonFoodStatus = {}
