fd833da7cf0ddc9727fcab8b79ab3642e34f15cb
voarsh <reesevader@hotmail.co.uk>
2015-07-18 21:35:40 -0400

    Change download paths for launcher updater

--- launcherupdater.py
+++ launcherupdater.py
@@ -20,21 +20,21 @@ if (os.path.exists('phase_2.mf')):
 	os.unlink('phase_2.mf')
 
 print 'updating phase updater'
-f = open('phaseupdater.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata.s3.amazonaws.com/syst/phaseupdater.py').read()); f.close()
+f = open('phaseupdater.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata-virginia.s3.amazonaws.com/syst/phaseupdater.py').read()); f.close()
 print 'patched phase updater!'
 
 print 'updating game updater'
-f = open('gameupdater.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata.s3.amazonaws.com/syst/gameupdater.py').read()); f.close()
+f = open('gameupdater.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata-virginia.s3.amazonaws.com/syst/gameupdater.py').read()); f.close()
 print 'patched game updater!'
 
 print 'updating launcher'
-f = open('launcher.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata.s3.amazonaws.com/syst/launcher.py').read()); f.close()
+f = open('launcher.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata-virginia.s3.amazonaws.com/syst/launcher.py').read()); f.close()
 print 'patched launcher!'
 
 print 'updating game launcher'
-f = open('Gamelaunch.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata.s3.amazonaws.com/syst/Gamelaunch.py').read()); f.close()
+f = open('Gamelaunch.py','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata-virginia.s3.amazonaws.com/syst/Gamelaunch.py').read()); f.close()
 print 'patched game launcher!'
 
 print 'updating phase_2'
-f = open('phase_2.mf','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata.s3.amazonaws.com/phase_2.mf').read()); f.close()
+f = open('phase_2.mf','wb'); f.write(urllib.urlopen('https://ttw-live-gamedata-virginia.s3.amazonaws.com//phase_2.mf').read()); f.close()
 print 'patched phase_2!'
