d9cbd1f29998217bc8ca7eec905d1715356d1545
daviddave <david.c.5@hotmail.co.uk>
2015-07-22 08:12:22 +0100

    revert remove . from addcells

--- toontown/toonbase/ToonBase.py
+++ toontown/toonbase/ToonBase.py
@@ -10,9 +10,8 @@ from direct.gui.DirectGui import *
 from direct.showbase.Transitions import Transitions
 from pandac.PandaModules import *
 from direct.interval.IntervalGlobal import Sequence, Func, Wait
-from otp.nametag.ChatBalloon import ChatBalloon
-from otp.nametag import NametagGlobals
-from otp.margins.MarginManager import MarginManager
+from toontown.margins.MarginManager import MarginManager
+from toontown.nametag import NametagGlobals
 import sys
 import os
 import math
@@ -48,7 +47,7 @@ class ToonBase(OTPBase.OTPBase):
         self.wantDynamicShadows = 0
         self.exitErrorCode = 0
         camera.setPosHpr(0, 0, 0, 0, 0, 0)
-        self.camLens.setMinFov(ToontownGlobals.DefaultCameraFov/(4./3.))
+        self.camLens.setFov(ToontownGlobals.DefaultCameraFov)
         self.camLens.setNearFar(ToontownGlobals.DefaultCameraNear, ToontownGlobals.DefaultCameraFar)
         self.cam2d.node().setCameraMask(BitMask32.bit(1))
         self.musicManager.setVolume(0.65)
@@ -158,6 +157,23 @@ class ToonBase(OTPBase.OTPBase):
         self.oldY = max(1, base.win.getYSize())
         self.aspectRatio = float(self.oldX) / self.oldY
         self.localAvatarStyle = None
+		# WASD option :D
+        self.wantWASD = self.display.settings.getBool('game', 'want-WASD', False)
+        
+        self.Move_Up = 'arrow_up'
+        self.Move_Left = 'arrow_left'       
+        self.Move_Down = 'arrow_down'
+        self.Move_Right = 'arrow_right'
+        self.JUMP = 'control'
+        
+        if self.wantWASD:
+            self.Move_Up = 'w'
+            self.Move_Left = 'a'            
+            self.Move_Down = 's'
+            self.Move_Right = 'd'
+            self.JUMP = 'space'
+        # Show cog levels on the battle GUI :D
+#        self.wantCogLevelGui = settings.get('want-ShowCogLevel', True)        
         return
 
     def openMainWindow(self, *args, **kw):
@@ -302,65 +318,48 @@ class ToonBase(OTPBase.OTPBase):
         self.screenshotStr += str
 
     def initNametagGlobals(self):
-        arrow = loader.loadModel('phase_3/models/props/arrow')
-        card = loader.loadModel('phase_3/models/props/panel')
-        speech3d = ChatBalloon(loader.loadModel('phase_3/models/props/chatbox'))
-        thought3d = ChatBalloon(loader.loadModel('phase_3/models/props/chatbox_thought_cutout'))
-        speech2d = ChatBalloon(loader.loadModel('phase_3/models/props/chatbox_noarrow'))
-        chatButtonGui = loader.loadModel('phase_3/models/gui/chat_button_gui')
-        NametagGlobals.setCamera(self.cam)
-        NametagGlobals.setArrowModel(arrow)
-        NametagGlobals.setNametagCard(card, VBase4(-0.5, 0.5, -0.5, 0.5))
-        if self.mouseWatcherNode:
-            NametagGlobals.setMouseWatcher(self.mouseWatcherNode)
-        NametagGlobals.setSpeechBalloon3d(speech3d)
-        NametagGlobals.setThoughtBalloon3d(thought3d)
-        NametagGlobals.setSpeechBalloon2d(speech2d)
-        NametagGlobals.setThoughtBalloon2d(thought3d)
-        NametagGlobals.setPageButton(PGButton.SReady, chatButtonGui.find('**/Horiz_Arrow_UP'))
-        NametagGlobals.setPageButton(PGButton.SDepressed, chatButtonGui.find('**/Horiz_Arrow_DN'))
-        NametagGlobals.setPageButton(PGButton.SRollover, chatButtonGui.find('**/Horiz_Arrow_Rllvr'))
-        NametagGlobals.setQuitButton(PGButton.SReady, chatButtonGui.find('**/CloseBtn_UP'))
-        NametagGlobals.setQuitButton(PGButton.SDepressed, chatButtonGui.find('**/CloseBtn_DN'))
-        NametagGlobals.setQuitButton(PGButton.SRollover, chatButtonGui.find('**/CloseBtn_Rllvr'))
+        NametagGlobals.setMe(base.cam)
+
+        NametagGlobals.setCardModel('phase_3/models/props/panel.bam')
+        NametagGlobals.setArrowModel('phase_3/models/props/arrow.bam')
+        NametagGlobals.setChatBalloon3dModel('phase_3/models/props/chatbox.bam')
+        NametagGlobals.setChatBalloon2dModel('phase_3/models/props/chatbox_noarrow.bam')
+        NametagGlobals.setThoughtBalloonModel('phase_3/models/props/chatbox_thought_cutout.bam')
+
+        chatButtonGui = loader.loadModel('phase_3/models/gui/chat_button_gui.bam')
+        NametagGlobals.setPageButton(
+            chatButtonGui.find('**/Horiz_Arrow_UP'), chatButtonGui.find('**/Horiz_Arrow_DN'),
+            chatButtonGui.find('**/Horiz_Arrow_Rllvr'), chatButtonGui.find('**/Horiz_Arrow_UP'))
+        NametagGlobals.setQuitButton(
+            chatButtonGui.find('**/CloseBtn_UP'), chatButtonGui.find('**/CloseBtn_DN'),
+            chatButtonGui.find('**/CloseBtn_Rllvr'), chatButtonGui.find('**/CloseBtn_UP'))
+        chatButtonGui.removeNode()
+
         rolloverSound = DirectGuiGlobals.getDefaultRolloverSound()
-        if rolloverSound:
+        if rolloverSound is not None:
             NametagGlobals.setRolloverSound(rolloverSound)
         clickSound = DirectGuiGlobals.getDefaultClickSound()
-        if clickSound:
+        if clickSound is not None:
             NametagGlobals.setClickSound(clickSound)
-        NametagGlobals.setToon(self.cam)
 
-
-        def addCell(self, x, y, a2dMarker):
-            cell = MarginCell()
-            cell.reparentTo(a2dMarker)
-            cell.setPos(x, 0, y)
-            cell.setScale(0.2)
-            cell.setActive(True)
-    
-            self.cells.add(cell)
-            self.reorganize()
-    
-            return cell
         self.marginManager = MarginManager()
         self.margins = self.aspect2d.attachNewNode(
             self.marginManager, DirectGuiGlobals.MIDGROUND_SORT_INDEX + 1)
         self.leftCells = [
-            addCell(0.1, -0.6, self.a2dTopLeft),
-            addCell(0.1, -1.0, self.a2dTopLeft),
-            addCell(0.1, -1.4, self.a2dTopLeft)
+            self.marginManager.addCell(0.1, -0.6, self.a2dTopLeft),
+            self.marginManager.addCell(0.1, -1.0, self.a2dTopLeft),
+            self.marginManager.addCell(0.1, -1.4, self.a2dTopLeft)
         ]
         self.bottomCells = [
-            addCell(0.4, 0.1, self.a2dBottomCenter),
-            addCell(-0.4, 0.1, self.a2dBottomCenter),
-            addCell(-1.0, 0.1, self.a2dBottomCenter),
-            addCell(1.0, 0.1, self.a2dBottomCenter)
+            self.marginManager.addCell(0.4, 0.1, self.a2dBottomCenter),
+            self.marginManager.addCell(-0.4, 0.1, self.a2dBottomCenter),
+            self.marginManager.addCell(-1.0, 0.1, self.a2dBottomCenter),
+            self.marginManager.addCell(1.0, 0.1, self.a2dBottomCenter)
         ]
         self.rightCells = [
-            addCell(-0.1, -0.6, self.a2dTopRight),
-            addCell(-0.1, -1.0, self.a2dTopRight),
-            addCell(-0.1, -1.4, self.a2dTopRight)
+            self.marginManager.addCell(-0.1, -0.6, self.a2dTopRight),
+            self.marginManager.addCell(-0.1, -1.0, self.a2dTopRight),
+            self.marginManager.addCell(-0.1, -1.4, self.a2dTopRight)
         ]
         
     def setCellsActive(self, cells, active):
@@ -368,27 +367,10 @@ class ToonBase(OTPBase.OTPBase):
             cell.setActive(active)
         self.marginManager.reorganize()
         
-
-    def hideFriendMargins(self):
-        middleCell = self.rightCells[1]
-        topCell = self.rightCells[2]
-
-        self.setCellsAvailable([middleCell, topCell], False)
-
-    def showFriendMargins(self):
-        middleCell = self.rightCells[1]
-        topCell = self.rightCells[2]
-
-        self.setCellsAvailable([middleCell, topCell], True)
-
-    def setCellsAvailable(self, cell_list, available):
-        for cell in cell_list:
-            self.marginManager.setCellAvailable(cell, available)
-
     def cleanupDownloadWatcher(self):
         self.downloadWatcher.cleanup()
         self.downloadWatcher = None
-        return
+        
 
     def startShow(self, cr, launcherServer = None):
         self.cr = cr
@@ -558,4 +540,3 @@ class ToonBase(OTPBase.OTPBase):
         wp = WindowProperties()
         wp.setMinimized(True)
         base.win.requestProperties(wp)
-
