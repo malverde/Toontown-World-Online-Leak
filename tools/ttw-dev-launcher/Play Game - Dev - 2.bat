fcec57c65e3c2604632836d94b3c65af5df18916
voarsh <reesevader@hotmail.co.uk>
2015-07-23 04:42:15 +0000

    Added dev launcher for pyd
    Has correct download links. Run bat and download the latest release
    candidate.

---
+++ tools/ttw-dev-launcher/Play Game - Dev - 2.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set /P ttrUsername="Username: " || ^
+set ttrUsername=username
+set ttrPassword=password
+set TTR_PLAYCOOKIE=%ttrUsername%
+set TTR_GAMESERVER=52.5.86.108
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+echo ===============================
+echo Starting Toontown World Online - DEV...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttrUsername%
+echo Client Agent IP: %TTR_GAMESERVER%
+echo ===============================
+
+%PPYTHON_PATH% -m toontown.toonbase.ToontownStart
+pause
---
+++ tools/ttw-dev-launcher/Play Game.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set TTR_GAMESERVER=52.0.191.143
+set /P PPYTHON_PATH=<PPYTHON_PATH
+%PPYTHON_PATH% -m launcher
+pause
