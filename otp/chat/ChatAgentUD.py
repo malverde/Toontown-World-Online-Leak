85ec6c225fe80ebd783a7e2d33c743889fa59b6c
daviddave <david.c.5@hotmail.co.uk>
2015-07-23 14:28:21 +0100

    help from TTO

--- otp/chat/ChatAgentUD.py
+++ otp/chat/ChatAgentUD.py
@@ -1,54 +1,41 @@
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.DistributedObjectGlobalUD import DistributedObjectGlobalUD
-# TODO: OTP should not depend on Toontown... Hrrm.
 from toontown.chat.TTWhiteList import TTWhiteList
-import time
+from otp.distributed import OtpDoGlobals
 
 class ChatAgentUD(DistributedObjectGlobalUD):
-    notify = DirectNotifyGlobal.directNotify.newCategory("ChatAgentUD")
+    notify = DirectNotifyGlobal.directNotify.newCategory('ChatAgentUD')
 
     def announceGenerate(self):
         DistributedObjectGlobalUD.announceGenerate(self)
+        self.wantWhitelist = config.GetBool('want-whitelist', True)
+        if self.wantWhitelist:
+            self.whiteList = TTWhiteList()
+        self.chatMode2channel = {1: OtpDoGlobals.OTP_MOD_CHANNEL,
+         2: OtpDoGlobals.OTP_ADMIN_CHANNEL,
+         3: OtpDoGlobals.OTP_SYSADMIN_CHANNEL}
+        self.chatMode2prefix = {1: '[MOD] ',
+         2: '[ADMIN] ',
+         3: '[SYSADMIN] '}
 
-        self.whiteList = TTWhiteList()
-        self.muted = {}
-    def muteAccount(self, account, howLong):
-        print ['muteAccount', account, howLong]
-        self.muted[account] = int(time.time()/60) + howLong
-
-    def unmuteAccount(self, account):
-        print ['unuteAccount', account]
-        if account in self.muted:
-            del self.muted[account]
     def chatMessage(self, message, chatMode):
         sender = self.air.getAvatarIdFromSender()
         if sender == 0:
-            self.air.writeServerEvent('suspicious', self.air.getAccountIdFromSender(),
-                                         'Account sent chat without an avatar', message)
+            self.air.writeServerEvent('suspicious', self.air.getAccountIdFromSender(), issue='Account sent chat without an avatar', message=message)
             return
-        if sender in self.muted and int(time.time()/60) < self.muted[sender]:
-            return
-
-        modifications = []
-        words = message.split(' ')
-        offset = 0
-        WantWhitelist = config.GetBool('want-whitelist', 1)
-
-        for word in words:
-            if word and not self.whiteList.isWord(word) and WantWhitelist:
-                modifications.append((offset, offset+len(word)-1))
-            offset += len(word) + 1
-
-        cleanMessage = message
-        for modStart, modStop in modifications:
-            cleanMessage = cleanMessage[:modStart] + '*'*(modStop-modStart+1) + cleanMessage[modStop+1:]
-
-        self.air.writeServerEvent('chat-said', sender, message, cleanMessage)
-
-        # TODO: The above is probably a little too ugly for my taste... Maybe AIR
-        # should be given an API for sending updates for unknown objects?
+        cleanMessage, modifications = message, []
+        self.air.writeServerEvent('chat-said', avId=sender, chatMode=chatMode, msg=message, cleanMsg=cleanMessage)
+        if chatMode != 0:
+            if message.startswith('.'):
+                cleanMessage = '.' + self.chatMode2prefix.get(chatMode, '') + message[1:]
+            else:
+                cleanMessage = self.chatMode2prefix.get(chatMode, '') + message
+            modifications = []
         DistributedAvatar = self.air.dclassesByName['DistributedAvatarUD']
-        dg = DistributedAvatar.aiFormatUpdate('setTalk', sender, sender,
-                                              self.air.ourChannel,
-                                              [0, 0, '', cleanMessage, modifications, 0])
+        dg = DistributedAvatar.aiFormatUpdate('setTalk', sender, self.chatMode2channel.get(chatMode, sender), self.air.ourChannel, [0,
+         0,
+         '',
+         cleanMessage,
+         modifications,
+         0])
         self.air.send(dg)
