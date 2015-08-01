bbf848de9ca2a16ea70c5db3f0fe408cb018d510
daviddave <david.c.5@hotmail.co.uk>
2015-08-01 16:11:41 +0100

    add this tunnels might work

--- toontown/toonbase/ToontownGlobals.py
+++ toontown/toonbase/ToontownGlobals.py
@@ -180,8 +180,8 @@ HoodHierarchy = {ToontownCentral: (SillyStreet, LoopyLane, PunchlinePlace),
  MinniesMelodyland: (AltoAvenue, BaritoneBoulevard, TenorTerrace),
  DaisyGardens: (ElmStreet, MapleStreet, OakStreet),
  DonaldsDreamland: (LullabyLane, PajamaPlace),
- OutdoorZone: (FunnyFarm),
- FunnyFarm: (OutdoorZone),
+ OutdoorZone: (),
+ FunnyFarm: (),
  GoofySpeedway: ()}
 WelcomeValleyToken = 0
 BossbotHQ = 10000
