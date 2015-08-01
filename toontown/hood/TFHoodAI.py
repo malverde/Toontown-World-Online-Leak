3a56204103d21f0bebc23dc8a7f83b968a544bf4
daviddave <david.c.5@hotmail.co.uk>
2015-08-01 08:57:12 +0100

    add cogs

--- toontown/hood/TFHoodAI.py
+++ toontown/hood/TFHoodAI.py
@@ -3,6 +3,7 @@ from toontown.hood.HoodAI import *
 from toontown.toonbase import ToontownGlobals
 from toontown.election import DistributedFlippyStandAI
 from toontown.election import DistributedToonfestTowerBaseAI
+from toontown.election import DistributedToonfestCogAI
 from toontown.safezone.DistributedPicnicBasketAI import DistributedPicnicBasketAI
 from toontown.safezone.DistributedPicnicTableAI import DistributedPicnicTableAI
 
@@ -15,3 +16,16 @@ class TFHoodAI(SZHoodAI):
         self.flippyStand.generateWithRequired(self.HOOD)
         self.toonfestTower = DistributedToonfestTowerBaseAI.DistributedToonfestTowerBaseAI(self.air)
         self.toonfestTower.generateWithRequired(self.HOOD)
+        self.cogs = []
+        
+    def createCogs(self):
+        posList = [[224,
+          -146,
+          4.597,
+          1]]
+        for pos in posList:
+            self.cog = DistributedToonfestCogAI.DistributedToonfestCogAI(self.air)
+            self.cog.generateWithRequired(self.HOOD)
+            self.cog.setPos(pos[0], pos[1], pos[2])
+            self.cog.setId(pos[3])
+            self.cogs.append(self.cog)
