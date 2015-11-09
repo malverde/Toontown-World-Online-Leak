4a48e12f88251be0c46a5567963675196605f00e
voarsh <reesevader@hotmail.co.uk>
2015-11-09 18:10:58 +0000

    Old 2.0.0 ChatAgent that seems to simply things - includes channels
    NEEDS TESTING!

--- otp/chat/ChatAgentUD.py
+++ otp/chat/ChatAgentUD.py
@@ -12,6 +12,7 @@ class ChatAgentUD(DistributedObjectGlobalUD):
 
         self.whiteList = TTWhiteList()
         self.muted = {}
+
     def muteAccount(self, account, howLong):
         print ['muteAccount', account, howLong]
         self.muted[account] = int(time.time()/60) + howLong
@@ -20,34 +21,40 @@ class ChatAgentUD(DistributedObjectGlobalUD):
         print ['unuteAccount', account]
         if account in self.muted:
             del self.muted[account]
+
+    self.chatMode2channel = {
+        1: OtpDoGlobals.OTP_MOD_CHANNEL,
+        2: OtpDoGlobals.OTP_ADMIN_CHANNEL,
+        3: OtpDoGlobals.OTP_SYSADMIN_CHANNEL,
+    }
+    self.chatMode2prefix = {
+        1: "[MOD] ",
+        2: "[ADMIN] ",
+        3: "[SYSADMIN] ",
+    }
+
     def chatMessage(self, message, chatMode):
         sender = self.air.getAvatarIdFromSender()
         if sender == 0:
             self.air.writeServerEvent('suspicious', self.air.getAccountIdFromSender(),
-                                         'Account sent chat without an avatar', message)
-            return
-        if sender in self.muted and int(time.time()/60) < self.muted[sender]:
+                                      issue='Account sent chat without an avatar', message=message)
             return
 
-        modifications = []
-        words = message.split(' ')
-        offset = 0
-        WantWhitelist = config.GetBool('want-whitelist', 1)
-        for word in words:
-            if word and not self.whiteList.isWord(word) and WantWhitelist:
-                modifications.append((offset, offset+len(word)-1))
-            offset += len(word) + 1
-
-        cleanMessage = message
-        for modStart, modStop in modifications:
-            cleanMessage = cleanMessage[:modStart] + '*'*(modStop-modStart+1) + cleanMessage[modStop+1:]
-
-        self.air.writeServerEvent('chat-said', sender, message, cleanMessage)
+        cleanMessage, modifications = message, []
+        self.air.writeServerEvent('chat-said', avId=sender, chatMode=chatMode, msg=message, cleanMsg=cleanMessage)
 
         # TODO: The above is probably a little too ugly for my taste... Maybe AIR
         # should be given an API for sending updates for unknown objects?
+        if chatMode != 0:
+            # Staff messages do not need to be cleaned. [TODO: Blacklist this?]
+            if message.startswith('.'):
+                # This is a thought bubble, move the point to the start.
+                cleanMessage = '.' + self.chatMode2prefix.get(chatMode, "") + message[1:]
+            else:
+                cleanMessage = self.chatMode2prefix.get(chatMode, "") + message
+            modifications = []
         DistributedAvatar = self.air.dclassesByName['DistributedAvatarUD']
-        dg = DistributedAvatar.aiFormatUpdate('setTalk', sender, sender,
+        dg = DistributedAvatar.aiFormatUpdate('setTalk', sender, self.chatMode2channel.get(chatMode, sender),
                                               self.air.ourChannel,
                                               [0, 0, '', cleanMessage, modifications, 0])
         self.air.send(dg)
