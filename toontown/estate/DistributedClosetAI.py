b061abef0d808ad073cc5138bedb7a26deee15f7
voarsh <reesevader@hotmail.co.uk>
2015-12-11 11:34:25 +0000

    Revert "Re-apply "aa09e05b2b8ef29aceadeb4bc8ffc9cec0abf65d" - fix ai crash on closing trunk"
    This reverts commit d5dcc795560025bb67c74b5a1995662323650543.

--- toontown/estate/DistributedClosetAI.py
+++ toontown/estate/DistributedClosetAI.py
@@ -220,7 +220,7 @@ class DistributedClosetAI(DistributedFurnitureItemAI, InteractableAI):
     def d_setMovie(self, movie, avId, time):
         self.sendUpdate('setMovie', [movie, avId, time])
 
-    def resetMovie(self):
+    def __resetMovie(self):
         self.d_setMovie(ClosetGlobals.CLOSET_MOVIE_CLEAR, 0, globalClockDelta.getRealNetworkTime())
 
     def setMovie(self, todo0, todo1, todo2):
