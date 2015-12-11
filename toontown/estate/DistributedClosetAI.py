2640f6aae2fda78f969fecca2d0f9547be817074
voarsh <reesevader@hotmail.co.uk>
2015-12-11 15:09:05 +0000

    Closet and Trunk seem to work now after removing __resetmovie - resetmovie

--- toontown/estate/DistributedClosetAI.py
+++ toontown/estate/DistributedClosetAI.py
@@ -145,7 +145,7 @@ class DistributedClosetAI(DistributedFurnitureItemAI, InteractableAI):
             return
         if finished == 1:
             self.d_setMovie(ClosetGlobals.CLOSET_MOVIE_COMPLETE, avId, globalClockDelta.getRealNetworkTime())
-            taskMgr.doMethodLater(1, self.__resetMovie, 'resetMovie-%d' % self.getDoId(), extraArgs=[])
+            taskMgr.doMethodLater(1, self.resetMovie, 'resetMovie-%d' % self.getDoId(), extraArgs=[])
             self.d_setState(ClosetGlobals.CLOSED, 0, self.furnitureMgr.ownerId, self.gender, self.topList, self.botList)
             av.b_setDNAString(self.customerDNA.makeNetString())
             self.removedBottoms = []
@@ -201,7 +201,7 @@ class DistributedClosetAI(DistributedFurnitureItemAI, InteractableAI):
             self.removedBottoms = []
             self.removedTops = []
             self.d_setMovie(ClosetGlobals.CLOSET_MOVIE_COMPLETE, avId, globalClockDelta.getRealNetworkTime())
-            taskMgr.doMethodLater(1, self.__resetMovie, 'resetMovie-%d' % self.getDoId(), extraArgs=[])
+            taskMgr.doMethodLater(1, self.resetMovie, 'resetMovie-%d' % self.getDoId(), extraArgs=[])
             self.d_setState(ClosetGlobals.CLOSED, 0, self.furnitureMgr.ownerId, self.gender, self.topList, self.botList)
             self.customerDNA = None
             self.avId = None
@@ -220,7 +220,7 @@ class DistributedClosetAI(DistributedFurnitureItemAI, InteractableAI):
     def d_setMovie(self, movie, avId, time):
         self.sendUpdate('setMovie', [movie, avId, time])
 
-    def __resetMovie(self):
+    def resetMovie(self):
         self.d_setMovie(ClosetGlobals.CLOSET_MOVIE_CLEAR, 0, globalClockDelta.getRealNetworkTime())
 
     def setMovie(self, todo0, todo1, todo2):
