a011a4301cbc94bc7a5e4c2d980cc3903fe3b7e0
voarsh <reesevader@hotmail.co.uk>
2015-12-20 03:20:20 +0000

    ChatAgentUD: Chat Channels work properly
    All messages are filtered regardless. I had to make 2.0.0 somewhat
    compatible with the 2.5.1/2.4.9/2.5.2 version.
    
    TODO: Get server event to say original msg and the cleaned version, as
    in 2.5.1 - this 2.0.0 version doesn't do it properly.

--- otp/chat/ChatAgentUD.py
+++ otp/chat/ChatAgentUD.py
@@ -38,16 +38,27 @@ class ChatAgentUD(DistributedObjectGlobalUD):
         sender = self.air.getAvatarIdFromSender()
         if sender == 0:
             self.air.writeServerEvent('suspicious', self.air.getAccountIdFromSender(),
-                                      issue='Account sent chat without an avatar', message=message)
+                                      'Account sent chat without an avatar', message)
             return
-
+        if sender in self.muted and int(time.time() / 60) < self.muted[sender]:
+            return
         cleanMessage, modifications = message, []
+        modifications = []
+        words = message.split(' ')
+        offset = 0
+        WantWhitelist = config.GetBool('want-whitelist', 1)
+        for word in words:
+            if word and not self.whiteList.isWord(word) and WantWhitelist:
+                modifications.append((offset, offset + len(word) - 1))
+            offset += len(word) + 1
+
+        # TODO: Get server event to say original msg and the cleaned version, as in 2.5.1 - this 2.0.0 version doesn't do it properly.
         self.air.writeServerEvent('chat-said', avId=sender, chatMode=chatMode, msg=message, cleanMsg=cleanMessage)
 
+        # V 2.0.0
         # TODO: The above is probably a little too ugly for my taste... Maybe AIR
         # should be given an API for sending updates for unknown objects?
         if chatMode != 0:
-            # Staff messages do not need to be cleaned. [TODO: Blacklist this?]
             if message.startswith('.'):
                 # This is a thought bubble, move the point to the start.
                 cleanMessage = '.' + self.chatMode2prefix.get(chatMode, "") + message[1:]
@@ -59,3 +70,4 @@ class ChatAgentUD(DistributedObjectGlobalUD):
                                               self.air.ourChannel,
                                               [0, 0, '', cleanMessage, modifications, 0])
         self.air.send(dg)
+
