8867305970da93658968836126718cde11663b79
voarsh <reesevader@hotmail.co.uk>
2015-12-24 22:25:31 +0000

    Fix NewsManagerAI - NewsManagerAI instance has no attribute FireworkName

--- toontown/ai/NewsManagerAI.py
+++ toontown/ai/NewsManagerAI.py
@@ -19,6 +19,7 @@ class NewsManagerAI(DistributedObjectAI):
 		self.accept('avatarEntered', self.__announceIfHoliday)
 		self.accept('avatarEntered', self.__announceIfFireworks)
 		self.HolidayManagerAI = HolidayManagerAI.HolidayManagerAI(air)
+		self.FireworkName = [ ]
 		day = str(datetime.datetime.now().strftime("%d"))
 		if str(datetime.datetime.now().strftime("%m")) == "12":
 			if day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30":
