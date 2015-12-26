29bdb73ad6b4fdd9d3eec943116a7fc2ae828542
daviddave <david.c.5@hotmail.co.uk>
2015-12-26 22:09:26 +0000

    Add 2.0 and skeletal cog invasions

--- toontown/ai/NewsInvasionAI.py
+++ toontown/ai/NewsInvasionAI.py
@@ -30,6 +30,7 @@ class NewsInvasionAI:
 
 	def InvTick(self, task):
 		day = str(datetime.datetime.now().strftime("%d"))
+		month = str(datetime.datetime.now().strftime("%m"))
 		task.delayTime = 1800
 		invMgr = simbase.air.suitInvasionManager
 		if invMgr.getInvading():
@@ -42,6 +43,41 @@ class NewsInvasionAI:
 				special = 0
 				invMgr.startInvasion(name, num,special)
 				print "Number Cruncher invasion spawned"
+			
+			elif month == "1" or month == "2" or month == "3" or month == "5" or month == "6" or month == "7" or month == "8" or month == "9" or month == "10" or month == "11" or month == "12" and day ==  "15":
+				name = 'ms'
+				num = 2500
+				special = 2
+				invMgr.startInvasion(name, num,special)
+				print "2.0 M&S invasion spawned"
+				
+			elif month == "1" or month == "3" or month == "5" or month == "7" or month == "8" or month == "10" or month == "12" and day ==  "31":
+				name = 'le'
+				num = 2500
+				special = 1
+				invMgr.startInvasion(name, num,special)
+				print "Skeletal legal eagle invasion spawned"
+				
+			elif month == "1" or month == "3" or month == "5" or month == "7" or month == "8" or month == "10" or month == "12" and day ==  "31":
+				name = 'le'
+				num = 2500
+				special = 1
+				invMgr.startInvasion(name, num,special)
+				print "Skeletal legal eagle invasion spawned"
+				
+			elif month == "4" or month == "6" or month == "9" or month == "11" and day ==  "30":
+				name = 'le'
+				num = 2500
+				special = 1
+				invMgr.startInvasion(name, num,special)
+				print "Skeletal legal eagle invasion spawned"
+				
+			elif month == "2" and day ==  "29":
+				name = 'le'
+				num = 2500
+				special = 1
+				invMgr.startInvasion(name, num,special)
+				print "Skeletal legal eagle invasion spawned"
 		
 		return task.again
 
