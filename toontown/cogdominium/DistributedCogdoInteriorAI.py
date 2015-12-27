9576011a9c1f702f6e09f44efa005543e0866a14
Daviddave <davidcrompton1@outlook.com>
2015-12-26 20:25:41 -0500

    just pass is. See if this stops the crash

--- toontown/cogdominium/DistributedCogdoInteriorAI.py
+++ toontown/cogdominium/DistributedCogdoInteriorAI.py
@@ -45,7 +45,7 @@ class DistributedCogdoInteriorAI(DistributedObjectAI, FSM.FSM):
         if len(npcIdList) == 0:
             if len(npcIdList) > 1:
                 pass
-            self.shopOwnerNpcId = NPCToons.npcFriends[1116]
+            pass
         self.gameDone = 0
         self.bossBattleDone = 0
         self.curFloor = 0
