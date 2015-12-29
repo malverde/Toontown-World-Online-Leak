c03bae47a6c2cbed08deb488d2160e6789bb6607
daviddave <david.c.5@hotmail.co.uk>
2015-12-29 20:22:11 +0000

    Add Halloween and Carroling. Add SQL/PHP based reporting

--- toontown/hood/DDHoodAI.py
+++ toontown/hood/DDHoodAI.py
@@ -3,6 +3,9 @@ from toontown.safezone.DistributedFishingSpotAI import DistributedFishingSpotAI
 from toontown.safezone.DistributedBoatAI import DistributedBoatAI
 from toontown.toon import NPCToons
 from SZHoodAI import SZHoodAI
+from toontown.ai import DistributedTrickOrTreatTargetAI
+from toontown.ai import DistributedWinterCarolingTargetAI
+import datetime
 
 
 class DDHoodAI(SZHoodAI):
@@ -15,3 +18,21 @@ class DDHoodAI(SZHoodAI):
 
         self.boat = DistributedBoatAI(self.air)
         self.boat.generateWithRequired(self.safezone)
+        day = str(datetime.datetime.now().strftime("%d"))
+        
+        if str(datetime.datetime.now().strftime("%m")) == "12" and day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30" or day == "31":
+            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
+            self.WinterCarolingTargetManager.generateWithRequired(1707)
+            
+        elif str(datetime.datetime.now().strftime("%m")) == "1" and day == "2" or day == "3" or day == "4":
+            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
+            self.WinterCarolingTargetManager.generateWithRequired(1707)
+            
+        elif str(datetime.datetime.now().strftime("%m")) == "10" and day ==  "21" or day == "22" or day == "23" or day == "25" or day == "26" or day == "27" or day == "28" or day == "29" or day == "30" or day == "31":
+            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
+            self.TrickOrTreatTargetManager.generateWithRequired(1834)
+            
+        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "1":
+            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
+            self.TrickOrTreatTargetManager.generateWithRequired(1834)
+
