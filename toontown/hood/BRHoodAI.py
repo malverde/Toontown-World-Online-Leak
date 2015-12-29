bffc4cb3d9d6835256f390884b9c742722c887d1
daviddave <david.c.5@hotmail.co.uk>
2015-12-29 23:00:31 +0000

    Revert "Add Halloween and Carroling. Add SQL/PHP based reporting"
    This reverts commit c03bae47a6c2cbed08deb488d2160e6789bb6607.

--- toontown/hood/BRHoodAI.py
+++ toontown/hood/BRHoodAI.py
@@ -1,10 +1,6 @@
 from toontown.toonbase import ToontownGlobals
 from SZHoodAI import SZHoodAI
 from toontown.toon import NPCToons
-from toontown.ai import DistributedPolarPlaceEffectMgrAI
-from toontown.ai import DistributedTrickOrTreatTargetAI
-from toontown.ai import DistributedWinterCarolingTargetAI
-import datetime
 
 
 class BRHoodAI(SZHoodAI):
@@ -14,25 +10,3 @@ class BRHoodAI(SZHoodAI):
         SZHoodAI.createZone(self)
 
         self.spawnObjects()
-        
-        day = str(datetime.datetime.now().strftime("%d"))
-        
-        self.PolarPlaceEffectManager = DistributedPolarPlaceEffectMgrAI.DistributedPolarPlaceEffectMgrAI(self.air)
-        self.PolarPlaceEffectManager.generateWithRequired(3821)
-       
-        
-        if str(datetime.datetime.now().strftime("%m")) == "12" and day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30" or day == "31":
-            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
-            self.WinterCarolingTargetManager.generateWithRequired(3828)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "1" and day == "2" or day == "3" or day == "4":
-            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
-            self.WinterCarolingTargetManager.generateWithRequired(3828)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "10" and day ==  "21" or day == "22" or day == "23" or day == "25" or day == "26" or day == "27" or day == "28" or day == "29" or day == "30" or day == "31":
-            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
-            self.TrickOrTreatTargetManager.generateWithRequired(3707)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "1":
-            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
-            self.TrickOrTreatTargetManager.generateWithRequired(3707)
--- toontown/hood/DLHoodAI.py
+++ toontown/hood/DLHoodAI.py
@@ -1,9 +1,6 @@
 from toontown.toonbase import ToontownGlobals
 from SZHoodAI import SZHoodAI
 from toontown.toon import NPCToons
-from toontown.ai import DistributedTrickOrTreatTargetAI
-from toontown.ai import DistributedWinterCarolingTargetAI
-import datetime
 
 
 class DLHoodAI(SZHoodAI):
@@ -13,21 +10,3 @@ class DLHoodAI(SZHoodAI):
         SZHoodAI.createZone(self)
 
         self.spawnObjects()
-        
-        day = str(datetime.datetime.now().strftime("%d"))
-        
-        if str(datetime.datetime.now().strftime("%m")) == "12" and day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30" or day == "31":
-            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
-            self.WinterCarolingTargetManager.generateWithRequired(9720)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "1" and day == "2" or day == "3" or day == "4":
-            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
-            self.WinterCarolingTargetManager.generateWithRequired(9720)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "10" and day ==  "21" or day == "22" or day == "23" or day == "25" or day == "26" or day == "27" or day == "28" or day == "29" or day == "30" or day == "31":
-            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
-            self.TrickOrTreatTargetManager.generateWithRequired(9619)
-            
-        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "1":
-            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
-            self.TrickOrTreatTargetManager.generateWithRequired(9619)
