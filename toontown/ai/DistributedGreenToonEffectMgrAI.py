39c059b7f7992a8493be2d70337a1c68477410c2
voarsh <reesevader@hotmail.co.uk>
2015-12-30 08:11:58 +0000

    Add Trick Or Treat mgr, Green Toon Effect, Res Emote, etc
    Update PolarPlaceEffect to use 103 phrase (Howdy!)

--- toontown/ai/DistributedGreenToonEffectMgrAI.py
+++ toontown/ai/DistributedGreenToonEffectMgrAI.py
@@ -1,10 +1,21 @@
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.DistributedObjectAI import DistributedObjectAI
+from direct.distributed.ClockDelta import *
+from direct.fsm.FSM import FSM
 
+class DistributedGreenToonEffectMgrAI(DistributedObjectAI,  FSM):
+    notify = DirectNotifyGlobal.directNotify.newCategory("DistributedGreenToonEffectMgrAI")
 
-class DistributedGreenToonEffectMgrAI(DistributedObjectAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory(
-        "DistributedGreenToonEffectMgrAI")
+    def __init__(self, air):
+        DistributedObjectAI.__init__(self, air)
+        FSM.__init__(self, 'GreenToonFSM')
+        self.air = air
+
+    def enterOff(self):
+        self.requestDelete()
 
     def addGreenToonEffect(self):
+        avId = self.air.getAvatarIdFromSender()
+        av = self.air.doId2do.get(avId)
+        av.b_setCheesyEffect(15, 0, 0)
         pass
--- toontown/ai/DistributedTrickOrTreatTarget.py
+++ toontown/ai/DistributedTrickOrTreatTarget.py
@@ -1,18 +1,20 @@
+from otp.speedchat import SpeedChatGlobals
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed import DistributedObject
-from otp.speedchat import SpeedChatGlobals
-import DistributedScavengerHuntTarget
+from direct.interval.IntervalGlobal import *
 
-
-class DistributedTrickOrTreatTarget(
-        DistributedScavengerHuntTarget.DistributedScavengerHuntTarget):
-    notify = DirectNotifyGlobal.directNotify.newCategory(
-        'DistributedTrickOrTreatTarget')
+class DistributedTrickOrTreatTarget(DistributedObject.DistributedObject):
+    notify = DirectNotifyGlobal.directNotify.newCategory('DistributedTrickOrTreatTarget')
 
     def __init__(self, cr):
-        DistributedScavengerHuntTarget.DistributedScavengerHuntTarget.__init__(
-            self,
-            cr)
+        DistributedObject.DistributedObject.__init__(self, cr)
+        self.triggered = False
+        self.triggerDelay = 15
+
+    def announceGenerate(self):
+        DistributedObject.DistributedObject.announceGenerate(self)
+        DistributedTrickOrTreatTarget.notify.debug('announceGenerate')
+        self.accept(SpeedChatGlobals.SCStaticTextMsgEvent, self.phraseSaid)
 
     def phraseSaid(self, phraseId):
         self.notify.debug('Checking if phrase was said')
@@ -23,9 +25,16 @@ class DistributedTrickOrTreatTarget(
 
         if phraseId == helpPhrase and not self.triggered:
             self.triggered = True
-            self.attemptScavengerHunt()
-            taskMgr.doMethodLater(
-                self.triggerDelay,
-                reset,
-                'ScavengerHunt-phrase-reset',
-                extraArgs=[])
+            self.d_requestScavengerHunt()
+            taskMgr.doMethodLater(self.triggerDelay, reset, 'ScavengerHunt-phrase-reset', extraArgs=[])
+
+    def delete(self):
+        self.ignore(SpeedChatGlobals.SCStaticTextMsgEvent)
+        DistributedObject.DistributedObject.delete(self)
+
+    def d_requestScavengerHunt(self):
+        self.sendUpdate('requestScavengerHunt', [])
+
+    def doScavengerHunt(self, amount):
+        DistributedTrickOrTreatTarget.notify.debug('doScavengerHunt')
+        base.localAvatar.trickOrTreatTargetMet(amount)
