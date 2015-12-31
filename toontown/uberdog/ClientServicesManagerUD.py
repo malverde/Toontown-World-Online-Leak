7e0182cfb4f4ab91132aa401fc2783c524f67270
Daviddave <davidcrompton1@outlook.com>
2015-12-31 17:05:35 -0500

    make everything a string. This shouldn't crash anymore reese

--- toontown/uberdog/ClientServicesManagerUD.py
+++ toontown/uberdog/ClientServicesManagerUD.py
@@ -972,5 +972,5 @@ class ClientServicesManagerUD(DistributedObjectGlobalUD):
             return
         #This connects to TTW.com and adds the entry in a DB table Reporter ID Reportee ID and the category of why they were reported
         connection = httplib.HTTPConnection("www.toontownworldonline.com")
-        connection.request("GET", "/api/csmud/report.php?reporterId="+ reporterId + "&avId=" + avId + "&category=" + REPORT_REASONS[category])
+        connection.request("GET", "/api/csmud/report.php?reporterId="+ str(reporterId) + "&avId=" + str(avId) + "&category=" + str(REPORT_REASONS[category]))
         self.air.writeServerEvent("player-reported", reporterId=reporterId, avId=avId, category=REPORT_REASONS[category])
