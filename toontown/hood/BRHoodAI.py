200af8931af5e59d1912af1d5aa0622cfed1fbdb
Daviddave <davidcrompton1@outlook.com>
2016-01-01 08:43:42 -0500

    Add 0

--- toontown/hood/BRHoodAI.py
+++ toontown/hood/BRHoodAI.py
@@ -25,7 +25,7 @@ class BRHoodAI(SZHoodAI):
             self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
             self.WinterCarolingTargetManager.generateWithRequired(3828)
             
-        elif str(datetime.datetime.now().strftime("%m")) == "1" and day == "2" or day == "3" or day == "4":
+        elif str(datetime.datetime.now().strftime("%m")) == "01" and day == "02" or day == "03" or day == "04":
             self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
             self.WinterCarolingTargetManager.generateWithRequired(3828)
             
@@ -33,6 +33,6 @@ class BRHoodAI(SZHoodAI):
             self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
             self.TrickOrTreatTargetManager.generateWithRequired(3707)
             
-        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "1":
+        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "01":
             self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
             self.TrickOrTreatTargetManager.generateWithRequired(3707)
