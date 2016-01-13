086fdcda7d647ce7c20870c914ad8c2dadf9456f
voarsh <reesevader@hotmail.co.uk>
2016-01-13 01:40:22 +0000

    BCalcAI: Budget fix for setbattleskip indentation problems

--- toontown/battle/BattleCalculatorAI.py
+++ toontown/battle/BattleCalculatorAI.py
@@ -1898,9 +1898,8 @@ class BattleCalculatorAI:
 
 @magicWord(category=CATEGORY_OVERRIDE, types=[int])
 def setBattleSkip(bs):
-    """ 
-    Globally causes any gag to do max damage to targeted cogs and never miss
-    """
+# Globally causes any gag to do max damage to targeted cogs and never miss
+
 	global battleSkip
 	battleSkip = bs
 	return "battleSkip = {0}".format(battleSkip)
