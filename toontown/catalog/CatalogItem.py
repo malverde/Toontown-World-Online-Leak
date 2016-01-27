357916aacf0f9544f95042de618e5294c1208367
voarsh <reesevader@hotmail.co.uk>
2016-01-27 20:42:30 +0000

    "Pandac" to "pandac" - typo fix - closes #65

--- toontown/catalog/CatalogItem.py
+++ toontown/catalog/CatalogItem.py
@@ -1,6 +1,6 @@
 # Embedded file name: toontown.catalog.CatalogItem
 from direct.directnotify import DirectNotifyGlobal
-from Pandac.PandaModules import *
+from pandac.PandaModules import *
 from toontown.toonbase import TTLocalizer
 from toontown.toonbase import ToontownGlobals
 from direct.interval.IntervalGlobal import *
