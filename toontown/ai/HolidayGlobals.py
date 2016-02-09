e3f2714e84728ac8124d2337db54a03f4095d8c5
Daviddave <davidcrompton1@outlook.com>
2016-02-09 19:39:57 +0100

    Add intergers and use <> to clean up code

--- toontown/ai/HolidayGlobals.py
+++ toontown/ai/HolidayGlobals.py
@@ -1,34 +1,34 @@
 import time
 import datetime
 
-day = str(datetime.datetime.now().strftime("%d"))
+day = int(datetime.datetime.now().strftime("%d"))
 
-if str(datetime.datetime.now().strftime("%m")) == "12" and day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30" or day == "31":
+if int(datetime.datetime.now().strftime("%m")) == 12 and day >= 14  and day <= 31:
 	HolidayName = 'Winter'
 else:
     HolidayName = 'None'
 
-if str(datetime.datetime.now().strftime("%m")) == "01" and day == "02" or day == "03" or day == "04":
+if str(datetime.datetime.now().strftime("%m")) == 1 and day >= 1 and day  <= 4:
     HolidayName = 'Winter'
 else:
     HolidayName = 'None'
 
-if str(datetime.datetime.now().strftime("%m")) == "10" and day ==  "21" or day == "22" or day == "23" or day == "25" or day == "26" or day == "27" or day == "28" or day == "29" or day == "30" or day == "31":
+if int(datetime.datetime.now().strftime("%m")) == 10 and day >= 21 and day <= 31:
     HolidayName = 'Halloween'
 else:
     HolidayName = 'None'
     
-if str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "01":
+if int(datetime.datetime.now().strftime("%m")) == 11 and day ==  1:
     HolidayName = 'Halloween'
 else:
     HolidayName = 'None'
 
-if str(datetime.datetime.now().strftime("%m")) == "03" and day ==  "29" or day == "30" or day == "31":
+if int(datetime.datetime.now().strftime("%m")) == 3 and day >= 29 and day <= 31:
     HolidayName = 'April Toons'
 else:
     HolidayName = 'None'
 
-if str(datetime.datetime.now().strftime("%m")) == "03" and day ==  "29" or day == "30" or day == "31":
+if int(datetime.datetime.now().strftime("%m")) == 4 and day >= 1 and day <= 11:
     HolidayName = 'April Toons'
 else:
     HolidayName = 'None'
