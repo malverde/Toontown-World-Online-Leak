8a7041b2b602ad7798291e04f1a464ba2559b1fa
Daviddave <davidcrompton1@outlook.com>
2016-02-09 20:24:18 +0100

    Re-enable NewsManager invasions

--- toontown/ai/NewsManagerAI.py
+++ toontown/ai/NewsManagerAI.py
@@ -5,7 +5,7 @@ from otp.ai.MagicWordGlobal import *
 from toontown.toonbase import TTLocalizerEnglish as TTLocalizer
 import datetime
 from toontown.ai import HolidayManagerAI
-#from toontown.ai import NewsInvasionAI
+from toontown.ai import NewsInvasionAI
 from toontown.ai import HolidayGlobals
 import time
 
@@ -16,8 +16,8 @@ class NewsManagerAI(DistributedObjectAI):
 		DistributedObjectAI.__init__(self, air)
 		self.accept('avatarEntered', self.__announceIfHoliday)
 		self.HolidayManagerAI = HolidayManagerAI.HolidayManagerAI(air)
-		#self.NewsInvasionAI = NewsInvasionAI.NewsInvasionAI(air)
-		#self.NewsInvasionAI.startInvTick()
+		self.NewsInvasionAI = NewsInvasionAI.NewsInvasionAI(air)
+		self.NewsInvasionAI.startInvTick()
 		self.HolidayName = []
 		
 	def __announceIfHoliday(self, avatar):
