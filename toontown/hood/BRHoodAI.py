de85d7f380e663ec4e87ccbaa6fcf65cc45dc0fe
Daviddave <davidcrompton1@outlook.com>
2016-02-11 22:51:34 +0100

    Add Holiday Globals

--- toontown/hood/BRHoodAI.py
+++ toontown/hood/BRHoodAI.py
@@ -4,7 +4,7 @@ from toontown.toon import NPCToons
 from toontown.ai import DistributedPolarPlaceEffectMgrAI
 from toontown.ai import DistributedTrickOrTreatTargetAI
 from toontown.ai import DistributedWinterCarolingTargetAI
-import datetime
+from toontown.ai import HolidayGlobals
 
 
 class BRHoodAI(SZHoodAI):
@@ -21,17 +21,10 @@ class BRHoodAI(SZHoodAI):
         self.PolarPlaceEffectManager.generateWithRequired(3821)
 
 
-        if str(datetime.datetime.now().strftime("%m")) == "12" and day == "14" or day == "15" or day == "16" or day == "17" or day == "18" or day == "19" or day == "20" or day == "21" or day == "22" or day == "23" or day == "24" or day == "25" or day == "26" or day == "27" or "28" or day == "29" or day == "30" or day == "31":
+        if HolidayGlobals.WhatHolidayIsIt() == 'Winter':
             self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
             self.WinterCarolingTargetManager.generateWithRequired(3828)
-
-        elif str(datetime.datetime.now().strftime("%m")) == "01" and day == "02" or day == "03" or day == "04":
-            self.WinterCarolingTargetManager = DistributedWinterCarolingTargetAI.DistributedWinterCarolingTargetAI(self.air)
-            self.WinterCarolingTargetManager.generateWithRequired(3828)
-
-        elif str(datetime.datetime.now().strftime("%m")) == "10" and day ==  "21" or day == "22" or day == "23" or day == "25" or day == "26" or day == "27" or day == "28" or day == "29" or day == "30" or day == "31":
-            self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
-            self.TrickOrTreatTargetManager.generateWithRequired(3707)
-        elif str(datetime.datetime.now().strftime("%m")) == "11" and day ==  "01":
+            
+        elif HolidayGlobals.WhatHolidayIsIt() == 'Halloween':
             self.TrickOrTreatTargetManager = DistributedTrickOrTreatTargetAI.DistributedTrickOrTreatTargetAI(self.air)
             self.TrickOrTreatTargetManager.generateWithRequired(3707)
