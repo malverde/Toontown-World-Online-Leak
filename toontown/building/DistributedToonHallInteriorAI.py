86830be7c91ecd266dd6307e5027e27255a8e34f
daviddave <david.c.5@hotmail.co.uk>
2016-03-13 18:00:31 +0000

    Add changes to interior AI and add logging

--- toontown/building/DistributedToonHallInteriorAI.py
+++ toontown/building/DistributedToonHallInteriorAI.py
@@ -1,7 +1,24 @@
-from direct.directnotify import DirectNotifyGlobal
-from toontown.building.DistributedToonInteriorAI import DistributedToonInteriorAI
+from DistributedToonInteriorAI import *
+from toontown.toonbase import ToontownGlobals
 
 
 class DistributedToonHallInteriorAI(DistributedToonInteriorAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory(
-        "DistributedToonHallInteriorAI")
+    def __init__(self, block, air, zoneId, building):
+        DistributedToonInteriorAI.__init__(self, block, air, zoneId, building)
+        self.accept('ToonEnteredZone', self.logToonEntered)
+        self.accept('ToonLeftZone', self.logToonLeft)
+
+    def logToonEntered(self, avId, zoneId):
+        result = self.getCurPhase()
+        if result == -1:
+            phase = 'not available'
+        else:
+            phase = str(result)
+        self.air.writeServerEvent(avId, 'entered toonhall')
+
+    def logToonLeft(self, avId, zoneId):
+        self.air.writeServerEvent(avId, 'exited toonhall')
+
+    def delete(self):
+        self.ignoreAll()
+        DistributedToonInteriorAI.delete(self)
